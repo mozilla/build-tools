46d826366166f441065938bfc37aa2b63c96853e
Axel Hecht <l10n@mozilla.com>
2007-12-26 14:22:47 +0100

    missing client.mk in repository for test app
    --HG--
    extra : convert_revision : a21bd8a7bc20bda13b0aa33f0ff1ee1b33178b40

---
+++ mozilla/client.mk
@@ -0,0 +1,6 @@
+# fake output here, we don't need this for anything else but
+# echo-variable-LOCALES_app and _toolkit, really.
+echo-variable-LOCALES_app:
+	@echo toolkit app
+echo-variable-LOCALES_toolkit:
+	@echo toolkit
