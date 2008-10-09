c202ac92c5751139f1e6f118a3747869da1c5253
Axel Hecht <l10n@mozilla.com>
2008-10-09 18:28:55 +0200

    refactoring tooling repo
    --HG--
    rename : mozilla/testing/tests/l10n/ez_setup.py => compare-locales/ez_setup.py
    rename : mozilla/testing/tests/l10n/lib/Mozilla/CompareLocales.py => compare-locales/lib/Mozilla/CompareLocales.py
    rename : mozilla/testing/tests/l10n/lib/Mozilla/Jars.py => compare-locales/lib/Mozilla/Jars.py
    rename : mozilla/testing/tests/l10n/lib/Mozilla/Parser.py => compare-locales/lib/Mozilla/Parser.py
    rename : mozilla/testing/tests/l10n/lib/Mozilla/Paths.py => compare-locales/lib/Mozilla/Paths.py
    rename : mozilla/testing/tests/l10n/lib/Mozilla/Tests.py => compare-locales/lib/Mozilla/Tests.py
    rename : mozilla/testing/tests/l10n/lib/Mozilla/__init__.py => compare-locales/lib/Mozilla/__init__.py
    rename : mozilla/testing/tests/l10n/scripts/compare-dirs => compare-locales/scripts/compare-dirs
    rename : mozilla/testing/tests/l10n/scripts/compare-locales => compare-locales/scripts/compare-locales
    rename : mozilla/testing/tests/l10n/scripts/compare-packs => compare-locales/scripts/compare-packs
    rename : mozilla/testing/tests/l10n/scripts/test-locales => compare-locales/scripts/test-locales
    rename : mozilla/testing/tests/l10n/scripts/verify-rss-redirects => compare-locales/scripts/verify-rss-redirects
    rename : mozilla/testing/tests/l10n/setup.py => compare-locales/setup.py
    rename : mozilla/testing/tests/l10n/tests/unitProperties.py => compare-locales/tests/unitProperties.py
    extra : convert_revision : 48c05985cd7d7d4b68be001757b4f39852fbd9f3

--- mozilla/testing/tests/l10n/extensions/coverage/coverage-extension-prefs.js
+++
@@ -1,3 +0,0 @@
-pref('javascript.options.showInConsole', true);
-pref('l10n.coverage.outdir', 'C:\\Sources\\screens');
-pref('testing.extensions.l10n.neterror-coverage', 'chrome://coverage/content/neterror-coverage.xul');
