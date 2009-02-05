216ef9cc16ba0b6fad45a29fae8d40ee2acf4b5e
Chris AtLee <catlee@mozilla.com>
2009-02-05 15:39:32 -0500

    Bug 432326 - Update web frontend and clobberer.py script. r=bhearsum
    Now we keep track of clobber times on a per-branch/builder/slave basis.
    Clobbering an entire branch or all slaves for a builder is accomplished through
    the web front end, by selecting all the related slaves.

--- clobberer/clobberer.py
+++ clobberer/clobberer.py
@@ -1,65 +1,114 @@
 import sys, shutil, urllib2, urllib, os
 from datetime import datetime, timedelta
-PERIODIC_CLOBBER_TIME = timedelta(days=7)
 
-def str_to_datetime(str):
-  return datetime.strptime(str, "%Y-%m-%d %H:%M:%S")
+def str_to_datetime(s):
+  return datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
 
 def datetime_to_str(dt):
   return dt.strftime("%Y-%m-%d %H:%M:%S")
 
-def write_file(dt, file):
-  if isinstance(dt, datetime):
-    dt = datetime_to_str(dt)
-  f = open(file, "w")
+def write_file(dt, fn):
+  assert isinstance(dt, datetime)
+  dt = datetime_to_str(dt)
+  f = open(fn, "w")
   f.write(dt)
   f.close()
 
-def do_clobber():
+def read_file(fn):
+  if not os.path.exists(fn):
+    return None
+
+  data = open(fn).read().strip()
   try:
-    if os.path.exists("build"):
-      print "Clobbering build directory"
-      shutil.rmtree("build")
+    return str_to_datetime(data)
+  except ValueError:
+    return None
+
+def do_clobber(dryrun=False, skip=None):
+  try:
+    for f in os.listdir("."):
+      if skip is not None and f in skip:
+        print "Skipping", f
+        continue
+      if os.path.isfile(f):
+        print "Removing", f
+        if not dryrun:
+          os.unlink(f)
+      elif os.path.isdir(f):
+        print "Removing %s/" % f
+        if not dryrun:
+          shutil.rmtree(f)
   except:
     print "Couldn't clobber properly, bailing out."
     sys.exit(1)
 
-url, = sys.argv[1:]
-url = urllib.quote(url, ":=?/~")
-
-try:
+def getClobberDate(baseURL, branch, builder, slave):
+  url = "%s?%s" % (baseURL,
+      urllib.urlencode(dict(branch=branch, builder=builder, slave=slave)))
   print "Checking clobber URL: %s" % url
-  cur_force_date = urllib2.urlopen(url).read()
-  print "Current forced clobber date: %s" % cur_force_date
-  if not os.path.exists("force-clobber"):
-    write_file(cur_force_date, "force-clobber")
-  else:
-    old_force_date = open("force-clobber").read()
-    print "Last forced clobber: %s" % old_force_date
-    if old_force_date != cur_force_date:
-      print "Clobber forced"
-      do_clobber()
-      write_file(cur_force_date, "force-clobber")
-      write_file(cur_force_date, "last-clobber")
-      sys.exit(0)
-except SystemExit:
-  # make sure to let sys.exit() through
-  pass
-except:
-  print "Couldn't poll %s, skipping forced clobber" % url
-  
-if not os.path.exists("last-clobber"):
-  write_file(datetime.utcnow(), "last-clobber")
-else:
-  last_clobber = str_to_datetime(open("last-clobber").read())
-  cur_date = datetime.utcnow()
-  print "Last clobber: %s" % datetime_to_str(last_clobber)
-  print "Current time: %s" % datetime_to_str(cur_date)
-  if (last_clobber + PERIODIC_CLOBBER_TIME < cur_date):
-    print "More than %s have passed since the last clobber, clobbering " % \
-      PERIODIC_CLOBBER_TIME
-    print "build directory"
-    do_clobber()
-    # if do_clobber fails the script will exit and this will not be executed
-    write_file(cur_date, "last-clobber")
+  data = urllib2.urlopen(url).read().strip()
+  try:
+    return str_to_datetime(data)
+  except ValueError:
+    return None
 
+if __name__ == "__main__":
+  from optparse import OptionParser
+  parser = OptionParser()
+  parser.add_option("-n", "--dry-run", dest="dryrun", action="store_true",
+      default=False, help="don't actually delete anything")
+  parser.add_option("-t", "--periodic", dest="period", type="float",
+      default=24*7, help="hours between periodic clobbers")
+  parser.add_option('-s', '--skip', help='do not delete this directory',
+      action='append', dest='skip', default=['last-clobber'])
+
+  options, args = parser.parse_args()
+  periodicClobberTime = timedelta(hours = options.period)
+
+  baseURL, branch, builder, slave = args
+
+  try:
+    server_clobber_date = getClobberDate(baseURL, branch, builder, slave)
+  except:
+    print "Error contacting server"
+    sys.exit(1)
+
+  our_clobber_date = read_file("last-clobber")
+
+  clobber = False
+
+  print "Our last clobber date: ", our_clobber_date
+  print "Server clobber date:   ", server_clobber_date
+
+  if server_clobber_date is not None:
+    # If the server is giving us a clobber date, compare the server's idea of
+    # the clobber date to our last clobber date
+    if our_clobber_date is None or server_clobber_date > our_clobber_date:
+      # If we've never been clobbered, or if the server's clobber date is greater
+      # than our last clobber date, then we should clobber.
+      clobber = True
+      # We should also update our clobber date to match the server's
+      our_clobber_date = server_clobber_date
+      print "Server is forcing a clobber"
+
+  if not clobber:
+    # Next, check if more than the periodicClobberTime period has passed since
+    # our last clobber
+    if our_clobber_date is None:
+      # We've never been clobbered
+      # Set our last clobber time to now, so that we'll clobber
+      # properly after periodicClobberTime
+      our_clobber_date = datetime.utcnow()
+      write_file(our_clobber_date, "last-clobber")
+    elif datetime.utcnow() > our_clobber_date + periodicClobberTime:
+      # periodicClobberTime has passed since our last clobber
+      clobber = True
+      # Update our clobber date to now
+      our_clobber_date = datetime.utcnow()
+      print "More than %s have passed since our last clobber" % periodicClobberTime
+
+  if clobber:
+    # Finally, perform a clobber if we're supposed to
+    print "Clobbering..."
+    do_clobber(options.dryrun, options.skip)
+    write_file(our_clobber_date, "last-clobber")
--- clobberer/index.php
+++ clobberer/index.php
@@ -1,14 +1,15 @@
-<!---
-This is a web interface that allows developers to clobber mozilla-central,
-actionmonkey, and tracemonkey builds.
+<?php
+/*
+This is a web interface that allows developers to clobber buildbot
+builds on a per-slave/per-builder basis.
 
 This script simply updates a database that Buildbot reads from at the start of
-every build. The BuildStep that does this is here:
-http://mxr.mozilla.org/mozilla/source/tools/buildbotcustom/steps/misc.py#167
--->
+every build.
+http://hg.mozilla.org/build/buildbotcustom/file/default/process/factory.py
+*/
 
-<?php
-$CLOBBERER_DB = '/var/www/html/build/clobberer/clobberer.db';
+$CLOBBERER_DB = '/var/www/html/build/stage-clobberer/db/clobberer.db';
+//$CLOBBERER_DB = '/var/www/html/build/clobberer/clobberer.db';
 
 $dbh = new PDO("sqlite:$CLOBBERER_DB");
 if (!$dbh) {
@@ -18,13 +19,25 @@ if (!$dbh) {
   exit(0);
 }
 
-$q = $dbh->query('SELECT count(*) FROM sqlite_master WHERE NAME="CLOBBERS"');
+$q = $dbh->query('SELECT count(*) FROM sqlite_master WHERE NAME="clobber_times"');
 $exists = $q->fetch(PDO::FETCH_NUM);
-if (!$exists[0]) {
-  $dbh->exec('CREATE TABLE CLOBBERS (short_name VARCHAR(30),'
-            .'long_name VARCHAR(50),'
-            .'lastclobber VARCHAR(30),'
-            .'clobberer VARCHAR(50))');
+if (!$exists or !$exists[0]) {
+  $res = $dbh->exec('CREATE TABLE clobber_times ('
+                   .'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                   .'branch VARCHAR(50),'
+                   .'builder VARCHAR(50),'
+                   .'slave VARCHAR(30),'
+                   .'lastclobber VARCHAR(30),'
+                   .'clobberer VARCHAR(50))');
+  if ($res === FALSE) {
+    die(print_r($dbh->errorInfo(), TRUE));
+  }
+  chmod($CLOBBERER_DB, 0660);
+}
+
+function b64_encode($s)
+{
+  return rtrim(base64_encode($s), "=");
 }
 
 function e($str)
@@ -33,54 +46,184 @@ function e($str)
   return $dbh->quote($str);
 }
 
+//
+// Handle form submission
+//
 if ($_POST['form_submitted']) {
   $clobbers = array();
+  $slaves = array();
   foreach ($_POST as $k => $v) {
     $t = explode('-', $k, 2);
-    if ($t[0] == 'clobber') {
-      array_push($clobbers, $t[1]);
+    // We only care about slave-<$row_id>
+    // This corresponds to a row that specifies which branch/builder/slave to clobber
+    if ($t[0] == 'slave') {
+      $row_id = e($t[1]);
+      $user = e($_SERVER['REMOTE_USER']);
+      $dbh->exec("UPDATE clobber_times SET lastclobber = DATETIME(\"NOW\"), clobberer = $user WHERE id = $row_id");
     }
   }
-  foreach ($clobbers as $c) {
-    $dbh->exec('UPDATE CLOBBERS SET lastclobber = DATETIME("NOW"), clobberer = ' . e($_SERVER['REMOTE_USER']) . ' WHERE short_name = ' . e($c));
-  }
+  // Redirect the user to the main page
+  // This prevents accidentally resubmitting the form if the user reloads the 
+  // page
+  header("Location: " . $_SERVER['REQUEST_URI']);
 }
 
-$tree = $_GET['tree'];
-if (!$tree) {
-  $alltrees = $dbh->query('SELECT short_name, long_name, lastclobber, clobberer FROM CLOBBERS ORDER BY long_name ASC');
+$builder = $_GET['builder'];
+$slave = $_GET['slave'];
+$branch = $_GET['branch'];
+// Show the administration page if no clobber time is being queried
+if (!$builder) {
 ?>
+<html>
 <head>
 <title>Mozilla Buildbot Clobberer</title>
+<link rel="stylesheet" href="clobberer.css" type="text/css" />
+<script src="jquery.min.js" language="javascript"></script>
+<script language="javascript">
+function toggleall(node, klass)
+{
+  if (klass) {
+    $("." + klass).attr("checked", node.checked);
+  }
+  var node_classes = $(node).attr("class").split(" ");
+  if (!node.checked) {
+    // If we just unchecked a node, then uncheck the parents too
+    for (var i = 0; i < node_classes.length; ++i) {
+      if (node_classes[i]) {
+        $("#" + node_classes[i]).attr("checked", false);
+      }
+    }
+  } else {
+    // If we just checked a node, then possibly check the parents if all
+    // children are checked
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 0; i < node_classes.length; ++i) {
+        if (node_classes[i]) {
+          // If none of this class is unchecked, then we can check the parent
+          if ($("." + node_classes[i] + ":not(:checked)").length == 0) {
+            var p = $("#" + node_classes[i]);
+            if (p && !p.attr("checked")) {
+              p.attr("checked", true);
+              // Loop through again if we just set our parent to checked
+              done = false;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
 </head>
+<body>
+<form method="POST">
 <table border="1" cellspacing="0" cellpadding="1">
- <form action="<?php print htmlspecialchars($_SERVER['REQUEST_URI']);?>" method="POST">
  <thead>
-  <tr><td>Name <td>Last Forced Clobber <td>Clobber?<td>Clobbered By
+  <tr><td>Branch</td><td>Builder Name</td><td>Slaves</td><td>Last clobbered</td></tr>
+ </thead>
  <tbody>
 <?php
-  while ($r = $alltrees->fetch(PDO::FETCH_ASSOC)) {
-    print '<tr><td>'.htmlspecialchars($r['long_name']).'<td>'.htmlspecialchars($r['lastclobber']);
-    print '<td><input type="checkbox" name="clobber-'.htmlspecialchars($r['short_name']).'">';
-    print '<td>'.htmlspecialchars($r['clobberer']);
+  $allbuilders = $dbh->query('SELECT * FROM clobber_times ORDER BY branch ASC, builder ASC');
+  if ($allbuilders) {
+    $last_branch = null;
+    $last_builder = null;
+    // First pass: count the number of rows for each branch / builder so we can 
+    // set the 'rowspan' attribute
+    $rows_per_branch = array();
+    $rows_per_builder = array();
+    $rows = array();
+    while ($r = $allbuilders->fetch(PDO::FETCH_ASSOC)) {
+      $rows[] = $r;
+      $builder = $r['builder'];
+      $branch = $r['branch'];
+      if (!array_key_exists($builder, $rows_per_builder)) {
+        $rows_per_builder[$builder] = 1;
+      } else {
+        $rows_per_builder[$builder] += 1;
+      }
+      if (!array_key_exists($branch, $rows_per_branch)) {
+        $rows_per_branch[$branch] = 1;
+      } else {
+        $rows_per_branch[$branch] += 1;
+      }
+    }
+    // Sort the results
+    function sort_func($r1, $r2) {
+      $c1 = strnatcmp($r1['branch'], $r2['branch']);
+      if ($c1 != 0) {
+        return $c1;
+      }
+      $c2 = strnatcmp($r1['builder'], $r2['builder']);
+      if ($c2 != 0) {
+        return $c2;
+      }
+      return strnatcmp($r1['slave'], $r2['slave']);
+    }
+    usort($rows, sort_func);
+    // Second pass we output the HTML
+    foreach ($rows as $r) {
+      print "<tr>";
+      if ($last_branch != $r['branch']) {
+        $branch_id = b64_encode($r['branch']);
+        $rowspan = $rows_per_branch[$r['branch']];
+        print "<td rowspan=\"$rowspan\">";
+        print "<input type=\"checkbox\" id=\"$branch_id\" onchange=\"toggleall(this, &quot;$branch_id&quot;)\" />";
+        print htmlspecialchars($r['branch']) . "</td>\n";
+      }
+      if ($last_builder != $r['builder']) {
+        $rowspan = $rows_per_builder[$r['builder']];
+        $builder_id = b64_encode($r['builder']);
+        $classes = b64_encode($r['branch']);
+        print "<td rowspan=\"$rowspan\"><input type=\"checkbox\" id=\"$builder_id\" class=\"$classes\" onchange=\"toggleall(this, &quot;$builder_id&quot;)\" />";
+        print htmlspecialchars($r['builder']) . "</td>\n";
+      }
+      $classes = b64_encode($r['builder']) . " " . b64_encode($r['branch']);
+      $name = "slave-" . $r['id'];
+      print "<td><input type=\"checkbox\" name=\"$name\" class=\"$classes\" onchange=\"toggleall(this)\" />";
+      print htmlspecialchars($r['slave']) . "</td>\n";
+      if ($r['lastclobber']) {
+        print "<td>" . htmlspecialchars($r['lastclobber']) . "(UTC) by " . htmlspecialchars($r['clobberer']) . "</td>\n";
+      } else {
+        print "<td></td>\n";
+      }
+      print "</tr>\n";
+      $last_branch = $r['branch'];
+      $last_builder = $r['builder'];
+    }
+  } else {
+    print "<tr><td colspan=\"9\">No data</td></tr>\n";
   }
 ?>
+ </tbody>
 </table>
 <input type="hidden" name="form_submitted" value="true">
 <input type="submit" value="Clobber now">
 </form>
+</body>
+</html>
 <?php
   exit(0);
 }
 
-$s = $dbh->query('SELECT lastclobber FROM CLOBBERS WHERE long_name = '.e($tree));
+// Handle requests from slaves asking about their last clobber date
+$e_builder = e($builder);
+$e_slave = e($slave);
+$e_branch = e($branch);
+$s = $dbh->query("SELECT id, lastclobber FROM clobber_times WHERE builder = $e_builder AND slave = $e_slave AND branch = $e_branch");
 $r = $s->fetch(PDO::FETCH_ASSOC);
 if (!$r) {
-  header('HTTP/1.x 500 Internal Server Error');
-  print "Couldn't get last clobber time for $tree";
+  // If this branch/builder/slave combination doesn't yet exist in the 
+  // database, then insert it
+  $res = $dbh->exec("INSERT INTO clobber_times (branch, builder, slave) VALUES ($e_branch, $e_builder, $e_slave)");
+  if ($res === false) {
+    header("HTTP/1.x 500 Internal Server Error");
+    print "Couldn't insert row<br/>\n";
+    die(print_r($dbh->errorInfo(), TRUE));
+  }
 }
 else {
-  header('HTTP/1.x 200 OK');
   print $r['lastclobber'];
 }
 ?>
