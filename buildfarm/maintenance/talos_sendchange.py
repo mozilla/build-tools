0357fd5e9785e2e8e5d6b227502973590a9e551b
Lukas Blakk <lsblakk@mozilla.com>
2010-07-21 22:38:30 -0700

    Adding a helper script for doing talos sendchanges for try server p=lsblakk r=aki

---
+++ buildfarm/maintenance/talos_sendchange.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# Created by Lukas Blakk on 2010-07-22
+"""talos_sendchange.py tryserverDir
+
+example usage:
+    python talos_sendchange.py mak77@bonardo.net-04da41d5f2ce
+
+This script creates and sends sendchanges for each of the
+PLATFORMS to each of the TEST_MASTERS"""
+
+import sys, os
+from ftplib import FTP
+
+TEST_MASTERS = ['talos-master02.build.mozilla.org:9012',
+                'test-master01.build.mozilla.org:9012',
+                'test-master02.build.mozilla.org:9012']
+PLATFORMS = ['linux', 'linux64', 'macosx', 'macosx64', 'win32']
+STAGE_BASE_PATH = '/pub/firefox/tryserver-builds/%(email)s-%(changeset)s/tryserver-%(platform)s/'
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    for master in TEST_MASTERS:
+        if len(args) > 0:
+            email,changeset = args[0].split('-')
+            ftp = FTP('ftp.mozilla.org')
+            ftp.login()
+
+            for platform in PLATFORMS:
+                tryserverUrlPath = STAGE_BASE_PATH % {'email': email, 
+                                                      'changeset': changeset, 
+                                                      'platform': platform}
+                filelist = ftp.nlst(tryserverUrlPath)
+
+                for f in filelist:
+                    for suffix in ('.tar.bz2', '.win32.zip', '.dmg'):
+                        if f.split(' ')[-1].endswith(suffix):
+                            sendchange = "buildbot sendchange --master %(master)s " \
+                                         "--branch tryserver-%(platform)s --revision %(changeset)s " \
+                                         "--user %(email)s http://stage.mozilla.org%(f)s" \
+                                         % {'master': master,
+                                            'platform': platform,
+                                            'changeset': changeset,
+                                            'email': email,
+                                            'f': f}
+                            print "Sending %s:%s" % (platform,master) 
+                            os.system(sendchange)
+                            print sendchange
+            ftp.quit()
+        else:
+            "Not enough arguments, you need to specify a tryserver dir (ie: email-changeset)"
