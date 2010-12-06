219caed6dbbc8bc06105cca2827b825337c823be
Chris AtLee <catlee@mozilla.com>
2010-12-06 17:01:32 -0500

    Bug 493791: script for compiling / running valgrind. r=bhearsum

---
+++ scripts/valgrind/valgrind.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+SCRIPTS_DIR="$(readlink -f $(dirname $0)/../..)"
+
+HERE=$(dirname $(readlink -f $0))
+if [ -z "$HG_REPO" ]; then
+    export HG_REPO="http://hg.mozilla.org/mozilla-central"
+fi
+
+if [ -f "$PROPERTIES_FILE" ]; then
+    PYTHON="/tools/python/bin/python"
+    JSONTOOL="$PYTHON $SCRIPTS_DIR/buildfarm/utils/jsontool.py"
+
+    builder=$($JSONTOOL -k properties.buildername $PROPERTIES_FILE)
+    slavename=$($JSONTOOL -k properties.slavename $PROPERTIES_FILE)
+    master=$($JSONTOOL -k properties.master $PROPERTIES_FILE)
+
+    builddir=$(basename $(readlink -f ..))
+    branch=$(basename $HG_REPO)
+
+    # Clobbering
+    if [ -z "$CLOBBERER_URL" ]; then
+        export CLOBBERER_URL="http://build.mozilla.org/clobberer"
+    fi
+
+    (cd ..;
+    python $SCRIPTS_DIR/clobberer/clobberer.py -s build $CLOBBERER_URL $branch \
+        $builder $builddir $slavename $master)
+
+    # Purging
+    (cd ..;
+    python $SCRIPTS_DIR/buildfarm/maintenance/purge_builds.py \
+        -s 4 -n info -n 'release-*' -n $builddir)
+fi
+
+python $SCRIPTS_DIR/buildfarm/utils/hgtool.py --tbox $HG_REPO src || exit 2
+if [ ! -d objdir ]; then
+    mkdir objdir
+fi
+cd objdir
+
+# Setup a basic mozconfig file
+echo '. $topsrcdir/browser/config/mozconfig' > mozconfig
+echo 'ac_add_options --enable-valgrind' >> mozconfig
+echo 'ac_add_options --with-ccache' >> mozconfig
+echo 'ac_add_options --disable-jemalloc' >> mozconfig
+echo 'CC=/tools/gcc-4.3.3/installed/bin/gcc' >> mozconfig
+echo 'CXX=/tools/gcc-4.3.3/installed/bin/g++' >> mozconfig
+
+make -f ../src/client.mk MOZCONFIG=$PWD/mozconfig configure || exit 2
+make -j4 || exit 2
+make package || exit 2
+
+debugger_args="--error-exitcode=1 --smc-check=all --gen-suppressions=all"
+suppression_file=$HERE/${MACHTYPE}.sup
+if [ -f $suppression_file ]; then
+    debugger_args="$debugger_args --suppressions=$suppression_file"
+fi
+
+export OBJDIR=.
+python _profile/pgo/profileserver.py --debugger=valgrind --debugger-args="$debugger_args" || exit 1
