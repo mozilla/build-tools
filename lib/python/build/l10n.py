2a186f230c27785f06145e266e845495b5f6c0dd
Rail Aliev <rail@mozilla.com>
2010-12-15 11:47:19 -0800

    Bug 619258 - Release l10n repacks fail due to missing env variable. r=bhearsum

--- lib/python/build/l10n.py
+++ lib/python/build/l10n.py
@@ -42,6 +42,6 @@ def repackLocale(locale, l10nRepoDir, l10nBaseRepo, revision, localeSrcDir,
     compareLocales(compareLocalesRepo, locale, l10nRepoDir, localeSrcDir,
                    l10nIni, revision=revision, merge=merge)
     env["AB_CD"] = locale
-    env["LOCALE_MERGEDIR"] = "%s/merged" % localeSrcDir
+    env["LOCALE_MERGEDIR"] = path.join(os.getcwd(), localeSrcDir, "merged")
     run_cmd(["make", "installers-%s" % locale], cwd=localeSrcDir, env=env)
     run_cmd(["make", "l10n-upload-%s" % locale], cwd=localeSrcDir, env=env)
