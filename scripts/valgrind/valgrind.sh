dd2f05fe6573e93791b757cf9f87a256927fe6b4
Phil Ringnalda <philringnalda@gmail.com>
2011-02-10 22:37:30 -0500

    Bug 631841: Move Valgrind suppression files into mozilla-central. r=catlee
    --HG--
    extra : rebase_source : 3ea7c6981f1320581799d8d7e2a1e9c7f38f9c27

--- scripts/valgrind/valgrind.sh
+++ scripts/valgrind/valgrind.sh
@@ -2,7 +2,6 @@
 set -e
 SCRIPTS_DIR="$(readlink -f $(dirname $0)/../..)"
 
-HERE=$(dirname $(readlink -f $0))
 if [ -z "$HG_REPO" ]; then
     export HG_REPO="http://hg.mozilla.org/mozilla-central"
 fi
@@ -58,7 +57,7 @@ make -j4 || exit 2
 make package || exit 2
 
 debugger_args="--error-exitcode=1 --smc-check=all --gen-suppressions=all --leak-check=full"
-suppression_file=$HERE/${MACHTYPE}.sup
+suppression_file=_valgrind/${MACHTYPE}.sup
 if [ -f $suppression_file ]; then
     debugger_args="$debugger_args --suppressions=$suppression_file"
 fi
