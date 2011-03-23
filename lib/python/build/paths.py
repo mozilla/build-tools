9d66dd18913024d99ae77c105c94a4b74eb157df
Rail Aliev <rail@mozilla.com>
2011-03-23 12:17:50 +0200

    Bug 639838 - Setup preproduction release master. r=bhearsum

--- lib/python/build/paths.py
+++ lib/python/build/paths.py
@@ -1,3 +1,7 @@
+import os
+from os import path
+
+
 def getLatestDir(product, branch, platform, nightlyDir="nightly",
                  protocol=None, server=None):
     if protocol:
@@ -21,3 +25,33 @@ def getMUSnippetDir(brandName, oldVersion, oldBuildNumber, version,
     return '%s-%s-build%s-%s-build%s-MU' % (brandName, oldVersion,
                                             oldBuildNumber, version,
                                             buildNumber)
+
+
+def getRealpath(file_name, depth=1, cwd=None):
+    """Returns real path of the file, resolving symlinks.
+
+    @type  file_name: string
+    @param file_name: File name to be resolved.
+
+    @type  depth: int
+    @param depth: How many directories to add to the path.
+                  /buids/buildbot/builder_master1/localconfig.py
+                  which points to
+                  /buids/buildbot/builder_master1/configs/mozilla/production_config.py
+                  will return mozilla/production_config.py with depth set to 1,
+                  and configs/mozilla/production_config.py with depth set to 2.
+
+    @type  cwd: string
+    @param cwd: Current working direcotry. Default is os.getcwd().
+    """
+
+    if not cwd:
+        cwd = os.getcwd()
+
+    if not path.isabs(file_name):
+        file_name = path.join(cwd, file_name)
+    real_path = path.realpath(file_name)
+    # Get N leading directories + file name
+    depth += 1
+    path_members = real_path.split(os.sep)[-depth:]
+    return path.join(*path_members)
