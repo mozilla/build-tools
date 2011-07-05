d654d41fc276759a0ba1904c76dfa1f8730eda4b
Nick Thomas <nthomas@mozilla.com>
2011-07-06 11:34:42 +1200

    Bug 602351, debug 'nightlies' should be copied into nightly/YYYY/MM/, r=lsblakk

--- buildfarm/maintenance/archive-debug-builds.sh
+++ buildfarm/maintenance/archive-debug-builds.sh
@@ -2,30 +2,35 @@
 
 BASE_PATH=/home/ftp/pub/mozilla.org/firefox/tinderbox-builds
 ARCHIVE_PATH=/home/ftp/pub/mozilla.org/firefox/nightly
-DATE_BASE=$(date +%Y-%m-%d-%H)
+DATE_BASE=$(date +%Y/%m)
 
 DIRS=$(ls $BASE_PATH | grep 'mozilla.*debug$')
 for dir in $DIRS
 do
   branch=$(echo $dir | cut -d '-' -f1,2)
   builddir="$BASE_PATH/$dir"
+  DATE_DIR=$(date +%Y-%m-%d-$branch-debug)
   cd $builddir
   archivedir="$(ls -r | head -1)"
-  if [ -n $archivedir ]; then
-    if [ -d "$builddir/$archivedir" ]; then
-      files="$(find $builddir/$archivedir/ -regex '.*\.\(dmg\|exe\|txt\|bz2\)')"
+  if [[ -n $archivedir && -d "$builddir/$archivedir" ]]; then
+    files="$(find $builddir/$archivedir/ -regex '.*\.\(dmg\|exe\|txt\|bz2\)')"
+    if [ -n "$files" ]; then
+      echo "Creating archive directory: $ARCHIVE_PATH/$DATE_BASE/$DATE_DIR"
+      mkdir -p "$ARCHIVE_PATH/$DATE_BASE/$DATE_DIR"
+      if [ ! -e $ARCHIVE_PATH/$DATE_DIR ]; then
+        ln -s $DATE_BASE/$DATE_DIR $ARCHIVE_PATH/$DATE_DIR
+      fi
       for file in $files
       do
         echo "Found recent nightly: $file"
         backup=$(basename $file | sed s/en-US\./en-US.debug-/)
         if [ -e $file ]; then
-          echo "Copying $file to $ARCHIVE_PATH/$DATE_BASE-$branch-debug/$backup"
-          mkdir -p "$ARCHIVE_PATH/$DATE_BASE-$branch-debug/"
-          cp -a $file "$ARCHIVE_PATH/$DATE_BASE-$branch-debug/$backup"
-        fi;
+          echo "Copying $file to $ARCHIVE_PATH/$DATE_BASE/$DATE_DIR/$backup"
+          cp -a $file "$ARCHIVE_PATH/$DATE_BASE/$DATE_DIR/$backup"
+        fi
       done
-    else
-      echo "skipping invalid dir"
-    fi;
-  fi;
-done;
+    fi
+  else
+    echo "skipping invalid dir"
+  fi
+done
