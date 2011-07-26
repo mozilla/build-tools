dd5e546d8962c65451497e77b06d7380d88a2620
Dustin J. Mitchell <dustin@mozilla.com>
2011-07-26 10:22:33 -0500

    Bug 666282; update clobberer cleanup.php to run from CLI SAPI; r=catlee
    --HG--
    rename : clobberer/cleanup_clobberer.sh => clobberer/cleanup_clobberer.cron

--- clobberer/cleanup.php
+++ clobberer/cleanup.php
@@ -3,45 +3,36 @@
 include("clobberer_creds.php");
 
 // disallow unless the correct token is given
-if ($_GET['pass'] != $CLEANUP_PASSWORD) {
-  header('HTTP/1.0 500 Internal Server Error');
-  echo "no.";
-  exit;
+if ($_ENV['CLEANUP_PASSWORD'] != $CLEANUP_PASSWORD) {
+  echo "wrong CLEANUP_PASSWORD\n";
+  exit(1);
 }
 
 $dbh = new PDO($CLOBBERER_DSN, $CLOBBERER_USERNAME,
         $CLOBBERER_PASSWORD, $CLOBBERER_PDO_OPTIONS);
 if (!$dbh) {
-  header('HTTP/1.0 500 Internal Server Error');
-  print("<h1>Error: couldn't connect</h1>");
+  print("Error: couldn't connect\n");
   print($error);
-  exit(0);
+  exit(1);
 }
 
 header("Content-type: text/plain");
 if (substr($CLOBBERER_DSN, 0, 5) == 'mysql') {
 	$q = "delete from builds where last_build_time < unix_timestamp(adddate(now(), interval -21 day)) and buildername not like 'rel%'";
-	echo "$q\n";
 	$dbh->exec($q) === false and die(print_r($dbh->errorInfo(), TRUE));
 
 	$q = "delete from clobber_times where lastclobber < unix_timestamp(adddate(now(), interval -21 day))";
-	echo "$q\n";
 	$dbh->exec($q) === false and die(print_r($dbh->errorInfo(), TRUE));
 
 	// optimize table?  Only reclaims table space, so probably not helpful..
 } else {
 	// sqlite
 	$q = "delete from builds where last_build_time < strftime('%s', 'now', '-21 days') and buildername not like 'rel%'";
-	echo "$q\n";
 	$dbh->exec($q) === false and die(print_r($dbh->errorInfo(), TRUE));
 
 	$q = "delete from clobber_times where lastclobber < strftime('%s', 'now', '-21 days')";
-	echo "$q\n";
 	$dbh->exec($q) === false and die(print_r($dbh->errorInfo(), TRUE));
 
 	$q = "vacuum";
-	echo "$q\n";
 	$dbh->exec($q) === false and die(print_r($dbh->errorInfo(), TRUE));
 }
-
-echo "OK";
--- clobberer/cleanup_clobberer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-CLEANUP_PASSWORD=sekrits
-wget "http://build.mozilla.org/clobberer/cleanup.php?pass=$CLEANUP_PASSWORD" 2>/dev/null
