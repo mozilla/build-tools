0d09055b0091e1d7b35c00091d7de23e23576a9e
Rail Aliev <rail@mozilla.com>
2011-07-27 14:31:41 -0400

    Bug 672162 - Disable Firefox 4.0/mozilla-2.0 nightlies; migrate users to a supported build. r=catlee

--- scripts/preproduction/repo_setup_config.py
+++ scripts/preproduction/repo_setup_config.py
@@ -7,17 +7,9 @@ repoSetupConfig['hgSshKey'] = 'ffxbld_dsa'
 repoSetupConfig['reposToClone'] = {
     'build/buildbot-configs': {
         'overrides': {
-            'mozilla/release-firefox-mozilla-1.9.1.py': [
-                'mozilla/preproduction_release_overrides.py',
-                'mozilla/preproduction_release_overrides-1.9.1.py',
-             ],
             'mozilla/release-firefox-mozilla-1.9.2.py': [
                 'mozilla/preproduction_release_overrides.py',
                 'mozilla/preproduction_release_overrides-1.9.2.py',
-             ],
-            'mozilla/release-firefox-mozilla-2.0.py': [
-                'mozilla/preproduction_release_overrides.py',
-                'mozilla/preproduction_release_overrides-2.0.py',
              ],
             'mozilla/release-firefox-mozilla-beta.py': [
                 'mozilla/preproduction_release_overrides.py',
