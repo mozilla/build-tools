a4429911a2987400e8c11d1e6105f40ede94b838
Chris Cooper <ccooper@deadsquid.com>
2011-07-27 10:45:48 -0400

    Bug 669899 - Use gcc 4.5 for spidermonkey - r=armenzg

--- scripts/spidermonkey_builds/spidermonkey.sh
+++ scripts/spidermonkey_builds/spidermonkey.sh
@@ -66,12 +66,12 @@ if [ "$OSTYPE" = "linux-gnu" ]; then
     UNAME_M=$(uname -m)
     MAKEFLAGS=-j4
     if [ "$UNAME_M" != "arm" ]; then
-        export CC=/tools/gcc-4.3.3/installed/bin/gcc
-        export CXX=/tools/gcc-4.3.3/installed/bin/g++
+        export CC=/tools/gcc-4.5/installed/bin/gcc
+        export CXX=/tools/gcc-4.5/installed/bin/g++
         if [ "$UNAME_M" = "x86_64" ]; then
-            export LD_LIBRARY_PATH=/tools/gcc-4.3.3/installed/lib64
+            export LD_LIBRARY_PATH=/tools/gcc-4.5/installed/lib64
         else
-            export LD_LIBRARY_PATH=/tools/gcc-4.3.3/installed/lib
+            export LD_LIBRARY_PATH=/tools/gcc-4.5/installed/lib
         fi
     fi
 fi
