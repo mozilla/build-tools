920c79c1bae8cf677572370009c9becb85dd0ea5
Rail Aliiev <rail@mozilla.com>
2012-02-14 16:43:17 -0500

    Bug 726508 - releases that use signing on demand end up with .gpg files in every locale directory in releases/. r=bhearsum

--- scripts/release/push-to-mirrors.py
+++ scripts/release/push-to-mirrors.py
@@ -36,6 +36,7 @@ DEFAULT_RSYNC_EXCLUDES = ['--exclude=*tests*',
                           '--exclude=*.checksums',
                           '--exclude=logs',
                           '--exclude=jsshell*',
+                          '--exclude=*/*.asc',
                           ]
 
 VIRUS_SCAN_CMD = ['nice', 'ionice', '-c2', '-n7',
