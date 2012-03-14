3fdadc17914907e0354a80b8a0ea0beab0ba2b9a
Nick Thomas <nthomas@mozilla.com>
2012-03-15 10:56:43 +1300

    Bug 735870, use ftp.m.o to download bits for try test jobs, r=aki

--- stage/post_upload.py
+++ stage/post_upload.py
@@ -21,7 +21,7 @@ CANDIDATES_URL_PATH = "http://stage.mozilla.org/pub/mozilla.org/%(product)s/%(ni
 PVT_BUILD_URL_PATH = "https://dm-pvtbuild01.mozilla.org/%(product)s/%(tinderbox_builds_dir)s"
 PVT_BUILD_DIR = "/mnt/pvt_builds/%(product)s/%(tinderbox_builds_dir)s"
 TRY_DIR = "/home/ftp/pub/%(product)s/try-builds/%(who)s-%(revision)s/%(builddir)s"
-TRY_URL_PATH = "http://stage.mozilla.org/pub/mozilla.org/%(product)s/try-builds/%(who)s-%(revision)s/%(builddir)s"
+TRY_URL_PATH = "http://ftp.mozilla.org/pub/mozilla.org/%(product)s/try-builds/%(who)s-%(revision)s/%(builddir)s"
 # Staging configs start here.  Uncomment when working on staging
 #TINDERBOX_URL_PATH = "http://dev-stage01.build.sjc1.mozilla.com/pub/mozilla.org/%(product)s/tinderbox-builds/%(tinderbox_builds_dir)s"
 #LONG_DATED_URL_PATH = "http://dev-stage01.build.sjc1.mozilla.com/pub/mozilla.org/%(product)s/%(nightly_dir)s/%(year)s/%(month)s/%(year)s-%(month)s-%(day)s-%(hour)s-%(minute)s-%(second)s-%(branch)s"
