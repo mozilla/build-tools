84cd2495cb18f8b91cd346a82ed61a4dc798bad9
Ben Hearsum <bhearsum@mozilla.com>
2012-03-22 14:48:57 -0400

    bug 734493: release tagging for a build 2 off default doesn't re-bump version. r=catlee

--- lib/python/build/versions.py
+++ lib/python/build/versions.py
@@ -11,6 +11,7 @@ ANY_VERSION_REGEX =\
     ('\d+\.\d[\d\.]*'    # A version number
     '([a-zA-Z]+\d+)?'    # Might be a project branch
     '((a|b)\d+)?'        # Might be an alpha or beta
+    '(esr)?'             # Might be an esr
     '(pre)?')            # Might be a 'pre' (nightly) version
 
 BUMP_FILES = {
--- lib/python/mozilla_buildtools/test/test_build_versions.py
+++ lib/python/mozilla_buildtools/test/test_build_versions.py
@@ -1,6 +1,26 @@
+import re
 import unittest
 
-from build.versions import bumpFile, nextVersion, BuildVersionsException
+from build.versions import bumpFile, nextVersion, BuildVersionsException, \
+  ANY_VERSION_REGEX
+
+class TestAnyVersionRegex(unittest.TestCase):
+    avr = '^%s$' % ANY_VERSION_REGEX
+
+    def testAlpha(self):
+        self.assertTrue(re.match(self.avr, '3.0a1'))
+
+    def testBeta(self):
+        self.assertTrue(re.match(self.avr, '3.0b12'))
+
+    def testEsr(self):
+        self.assertTrue(re.match(self.avr, '10.0.4esr'))
+
+    def testEsrPre(self):
+        self.assertTrue(re.match(self.avr, '10.0.5esrpre'))
+
+    def testBad(self):
+        self.assertFalse(re.match(self.avr, '3.0c'))
 
 class TestBuildVersions(unittest.TestCase):
     def _doTest(self, original, expected):
