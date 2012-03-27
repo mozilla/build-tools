89719526b7bacfdf5417ae5d9e4e0ee6755f0d2c
Chris AtLee <catlee@mozilla.com>
2012-03-26 21:49:13 -0400

    Bug 734764: Set directory permissions explicitly to avoid umask issues. r=rail

--- lib/python/mozilla_buildtools/queuedir.py
+++ lib/python/mozilla_buildtools/queuedir.py
@@ -50,10 +50,15 @@ class QueueDir(object):
 
     def setup(self):
         for d in (self.tmp_dir, self.new_dir, self.cur_dir, self.log_dir, self.dead_dir):
-            if not os.path.exists(d):
-                os.makedirs(d, 0755)
-            else:
-                os.chmod(d, 0755)
+            # Create our directories a bit at a time so we can make sure the
+            # modes are created properly
+            parts = d.split("/")
+            path = "/"
+            for p in parts:
+                path = os.path.join(path, p)
+                if not os.path.exists(path):
+                    os.mkdir(path)
+                    os.chmod(path, 0755)
 
         self.cleanup()
 
