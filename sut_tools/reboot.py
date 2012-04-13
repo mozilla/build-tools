f1d4994bc83ac4e4c5422ef530a42f5fe71aeffa
Justin Wood <callek@mozilla.com>
2012-04-13 17:42:59 -0400

    Bug 746042 - Clientproxy should ensure buildbot stops after every job run on a tegra. r=bear
    --HG--
    extra : rebase_source : 74dfafe71c56a697ab4312f1c89f6a699aa2259d

--- sut_tools/reboot.py
+++ sut_tools/reboot.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os, sys
 import devicemanagerSUT as devicemanager
 import socket
 import random
 import time
+import datetime
 from sut_lib import getOurIP, calculatePort, clearFlag, setFlag, waitForDevice
+from sut_lib import getLastLine
 
 if (len(sys.argv) <> 2):
   print "usage: reboot.py <ip address>"
@@ -15,8 +20,12 @@ if (len(sys.argv) <> 2):
 cwd       = os.getcwd()
 proxyFile = os.path.join(cwd, '..', 'proxy.flg')
 errorFile = os.path.join(cwd, '..', 'error.flg')
+forceRebootFile = os.path.join(cwd, '..', 'forceReboot.flg')
 proxyIP   = getOurIP()
 proxyPort = calculatePort()
+
+stringedNow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+setFlag(forceRebootFile, stringedNow)
 
 print "connecting to: %s" % sys.argv[1]
 dm = devicemanager.DeviceManagerSUT(sys.argv[1])
@@ -35,11 +44,10 @@ finally:
     try:
         waitForDevice(dm, waitTime=600)
     except SystemExit:
-        clearFlag(proxyFile)
         setFlag(errorFile, "Remote Device Error: call for device reboot failed")
-        sys.exit(1)
-        
     clearFlag(proxyFile)
+
+time.sleep(20*60) # Let Buildbot Die or ForcedRebootFlag kill us
 
 #if status is None or not status:
 #    print "Remote Device Error: call for device reboot failed"
