98313197f37576565183d8f447b8e95ce124e12f
Justin Wood <callek@mozilla.com>
2012-05-08 19:51:13 -0400

    Bug 752966 - Foopy kill_stalled.sh does not properly kill bcontroller.py. r=bear
    --HG--
    extra : rebase_source : adab0cb0292c5544990e2971eb2a2cdffc743425

--- buildfarm/mobile/kill_stalled.sh
+++ buildfarm/mobile/kill_stalled.sh
@@ -8,15 +8,8 @@ if [ -z $1 ] ; then
 else
   for tegra in $* ; do
     if [ -d tegra-$tegra ] ; then
-      stalled=`ps auxw | grep server.js | grep tegra-${tegra} | awk '{print $2}'`
-      if [ ! -z $stalled ] ; then
-        kill ${stalled}
-        echo tegra-${tegra} stalled server.js at PID ${stalled} killed
-        now=`date +%Y%m%d%H%M%S`
-        echo ${now},tegra-${tegra},stalled >> /builds/tegra_events.log
-      else
-        echo tegra-${tegra} none found
-      fi
+      echo killing any stray processes from tegra-$tegra
+      python tegra_checkstalled.py tegra-$tegra
     else
       echo skipping tegra-${tegra}
     fi
