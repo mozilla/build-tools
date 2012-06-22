620ca51fc73a0d89fc859228842c76e01a22bd23
Justin Wood <Callek@gmail.com>
2012-06-21 21:39:26 -0400

    Bug 726176 - Make start_cp.sh able to launch stuff right on linux. r=bear for "setup linux foopy to test running clientproxy on"

--- buildfarm/mobile/start_cp.sh
+++ buildfarm/mobile/start_cp.sh
@@ -1,8 +1,15 @@
-#!/bin/sh -x
+#!/bin/sh
 cd /builds
 
+# Use facter to distinguish between puppetized linux and mac
+if [ -n "`facter hostname 2>/dev/null`" ] ; then
+   . /tools/buildbot/bin/activate
+fi
+
+set -x
+
 if [ "$TERM" != "screen" ] ; then
-  "echo ERROR: Must run while attached to a screen. Use \|screen -x\|.""
+  "echo ERROR: Must run while attached to a screen. Use \|screen -x\|."
   exit 1
 fi
 
@@ -24,6 +31,12 @@ for i in ${tegras}; do
     cd ..
   fi
 done
+
+set +x
+
+if [ -n "`facter hostname 2>/dev/null`" ] ; then
+  deactivate
+fi
 
 echo start_cp.sh finished, remember to DETATCH from your screen session not
 echo quit it! Do so correctly with: C-a d
