87dcdc02b0eb922eeb4aef2931c135e3b63b30d0
Justin Wood <Callek@gmail.com>
2012-09-11 19:32:08 -0400

    Bug 789474 - update sutagent to 1.13 for tegra automation. r=aki

--- sut_tools/updateSUT.py
+++ sut_tools/updateSUT.py
@@ -11,7 +11,7 @@ import os
 from sut_lib import connect
 
 # Constants
-target_version = "1.12"
+target_version = "1.13"
 apkfilename = "sutAgentAndroid.apk"
 tegra_name = os.getenv('SUT_NAME')
 apkFoopyDirPattern =  "/builds/%(tegra_name)s"
