1e0e51f2bea47e348afd8ad9f7fed9b188ece805
Justin Wood <Callek@gmail.com>
2012-10-05 22:46:11 -0400

    regression fix - Actually check for stalled processes in verify/cleanup. r=philor, pending-f=jmaher FROM: Bug 795074 - refactor sut_tools to be more flexible and optionally do parts

--- sut_tools/verify.py
+++ sut_tools/verify.py
@@ -243,7 +243,7 @@ def setWatcherINI(dm):
     return False
 
 
-def verifyDevice(tegra, checksut=False, doCheckStalled=False, watcherINI=False):
+def verifyDevice(tegra, checksut=False, doCheckStalled=True, watcherINI=True):
     # Returns False on failure, True on Success
     global dm, errorFile
     tegraPath = os.path.join('/builds', tegra)
