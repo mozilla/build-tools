17a24b297c73bb4e57383fd8d17f0d0aee6caa25
Justin Wood <Callek@gmail.com>
2012-12-19 17:22:12 -0500

    Bug 822534 - Coerce manage_foopy fabric script to accept individual devices as well. r=armenzg

--- buildfarm/maintenance/manage_foopies.py
+++ buildfarm/maintenance/manage_foopies.py
@@ -7,6 +7,51 @@ from Crypto.Random import atfork
 
 FAIL = red('[FAIL]')
 
+_devices_map = dict()
+_foopy_map = dict()
+def _calc_maps(devices_file):
+    global _devices_map, _foopy_map
+    all_devices = json.load(urllib.urlopen(devices_file))
+    _devices_map = dict([[x, all_devices[x]] for x in all_devices
+                                   if all_devices[x].has_key('foopy')
+                                   and all_devices[x]['foopy'] is not None])
+    # Extract foopies from devices
+    all_foopies = [all_devices[x]['foopy'] for x in all_devices
+                                           if all_devices[x].has_key('foopy')]
+    # Use set to trim dupes
+    all_foopies = set(all_foopies) - set(["None"])
+    # For sanity when using -H all, we revert to list and sort
+    all_foopies = [x for x in all_foopies]
+    all_foopies.sort()
+    for foopy in all_foopies:
+        _foopy_map[foopy] = dict([[x, _devices_map[x]] for x in _devices_map
+                                             if _devices_map[x]['foopy'] == foopy])
+
+def _calc_selected(hosts, devices):
+    global _foopy_map, _devices_map
+    from copy import deepcopy
+    selected = dict()
+    if 'all' in hosts or 'all' in devices:
+        selected = deepcopy(_foopy_map)
+        return selected
+
+    for f in hosts:
+        if _foopy_map.has_key(f):
+            selected[f] = deepcopy(_foopy_map[f])
+    for d in devices:
+        if not _devices_map.has_key(d):
+            continue
+        if not _devices_map[d].has_key('foopy'):
+            continue
+        if not _foopy_map.has_key(_devices_map[d]['foopy']):
+            continue
+        if selected.has_key(_devices_map[d]['foopy']):
+            selected[_devices_map[d]['foopy']][d] = deepcopy(d)
+        else: # No mapping for this foopy
+            selected[_devices_map[d]['foopy']] = dict()
+            selected[_devices_map[d]['foopy']][d] = deepcopy(d)
+    return selected
+
 def run_action_on_foopy(action, foopy):
     atfork()
     try:
@@ -23,6 +68,9 @@ def run_action_on_foopy(action, foopy):
         print traceback.format_exc()
         return False
 
+def run_action_on_devices(action, foopy_dict):
+    print FAIL, "Per-Device Actions not yet supported"
+
 if __name__ == '__main__':
     from optparse import OptionParser
     import sys
@@ -36,10 +84,12 @@ if __name__ == '__main__':
     
     parser.set_defaults(
         hosts=[],
+        devices=[],
         concurrency=1,
         )
     parser.add_option("-f", "--devices-file", dest="devices_file", help="list/url of devices.json")
     parser.add_option("-H", "--host", dest="hosts", action="append")
+    parser.add_option("-D", "--device", dest="devices", action="append")
     parser.add_option("-j", dest="concurrency", type="int")
 
     options, actions = parser.parse_args()
@@ -53,37 +103,30 @@ if __name__ == '__main__':
     if not actions:
         parser.error("at least one action is required")
 
-    all_devices = json.load(urllib.urlopen(options.devices_file))
-    # Extract foopies from devices
-    all_foopies = [all_devices[x]['foopy'] for x in all_devices
-                                           if all_devices[x].has_key('foopy')]
-    # Use set to trim dupes
-    all_foopies = set(all_foopies) - set(["None"])
-    # For sanity when using -H all, we revert to list and sort
-    all_foopies = [x for x in all_foopies]
-    all_foopies.sort()
-    
-    selected_foopies = []
-    for f in all_foopies:
-        if f in options.hosts:
-            selected_foopies.append(f)
-        elif 'all' in options.hosts:
-            selected_foopies.append(f)
-    
-    if len(selected_foopies) == 0:
-        parser.error("You need to specify a foopy via -H")
+    _calc_maps(options.devices_file)
+
+    selected = _calc_selected(options.hosts, options.devices)
+
+    if len(selected.keys()) == 0:
+        parser.error("You need to specify a foopy via -H or a device via -D")
     
     env.user = 'cltbld'
     
     for action in actions:
+        fn = run_action_on_foopy
+        genArg = lambda x: x
+        if hasattr(getattr(foopy_fabric, action), "per_device"):
+            fn = run_action_on_devices
+            genArg = lambda x: {'host': x, 'devices': selected[x]}
+
         if options.concurrency == 1:
-            for foopy in selected_foopies:
-                run_action_on_foopy(action, foopy)
+            for foopy in sorted(selected.keys()):
+                fn(action, genArg(foopy))
         else:
             p = multiprocessing.Pool(processes=options.concurrency)
             results = []
-            for foopy in selected_foopies:
-                result = p.apply_async(run_action_on_foopy, (action, foopy) )
+            for foopy in sorted(selected.keys()):
+                result = p.apply_async(fn, (action, genArg(foopy)) )
                 results.append( (foopy, result) )
             p.close()
             failed = False
@@ -94,4 +137,3 @@ if __name__ == '__main__':
             p.join()
             if failed:
                 sys.exit(1)
-
