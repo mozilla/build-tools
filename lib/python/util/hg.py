4346f1ae9d7d458f22b9ab8aa3941596a71e64c0
Rail Aliiev <rail@mozilla.com>
2013-02-13 14:07:45 -0500

    Bug 838926 - Release runner fails to strip outgoing revs on bm36. r=bhearsum

--- lib/python/util/hg.py
+++ lib/python/util/hg.py
@@ -483,7 +483,8 @@ def apply_and_push(localrepo, remote, changer, max_attempts=10,
             if n == max_attempts:
                 log.debug("Tried %d times, giving up" % max_attempts)
                 for r in reversed(new_revs):
-                    run_cmd(['hg', 'strip', '-n', r[REVISION]], cwd=localrepo)
+                    run_cmd(['hg', '--config', 'extensions.mq=', 'strip', '-n',
+                             r[REVISION]], cwd=localrepo)
                 raise HgUtilError("Failed to push")
             pull(remote, localrepo, update_dest=False,
                  ssh_username=ssh_username, ssh_key=ssh_key)
@@ -496,7 +497,8 @@ def apply_and_push(localrepo, remote, changer, max_attempts=10,
                 log.debug("Failed to rebase: %s" % str(e))
                 update(localrepo, branch=branch)
                 for r in reversed(new_revs):
-                    run_cmd(['hg', 'strip', '-n', r[REVISION]], cwd=localrepo)
+                    run_cmd(['hg', '--config', 'extensions.mq=', 'strip', '-n',
+                             r[REVISION]], cwd=localrepo)
                 changer(localrepo, n + 1)
 
 
@@ -512,7 +514,8 @@ def cleanOutgoingRevs(reponame, remote, username, sshKey):
                                           ssh_username=username,
                                           ssh_key=sshKey))
     for r in reversed(outgoingRevs):
-        run_cmd(['hg', 'strip', '-n', r[REVISION]], cwd=reponame)
+        run_cmd(['hg', '--config', 'extensions.mq=', 'strip', '-n',
+                 r[REVISION]], cwd=reponame)
 
 
 def path(src, name='default'):
