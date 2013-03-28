d929e5b05aa2f3b9667d3df6488aab76e9ebf289
Chris Cooper <ccooper@deadsquid.com>
2013-03-28 12:29:37 -0400

    Bug 810736 - use bundle when set in ENV - r=armenzg

--- scripts/fuzzing/fuzzer.sh
+++ scripts/fuzzing/fuzzer.sh
@@ -7,11 +7,13 @@ trap "ssh-agent -k" EXIT
 SCRIPTS_DIR="$(dirname $0)/../.."
 
 # Call the Python 2.7 package in Win64 machines.
-if [ $OS = "Windows_NT" ] && [ -e "/c/mozilla-build/python27/python.exe" ]; then
+if [ "$OS" = "Windows_NT" ] && [ -e "/c/mozilla-build/python27/python.exe" ]; then
     PYBIN="/c/mozilla-build/python27/python.exe"
 else
     PYBIN="python"
 fi
 
-$PYBIN $SCRIPTS_DIR/buildfarm/utils/hgtool.py $HG_REPO fuzzing
+test $HG_BUNDLE && BUNDLE_ARGS="--bundle $HG_BUNDLE"
+
+$PYBIN $SCRIPTS_DIR/buildfarm/utils/hgtool.py $BUNDLE_ARGS $HG_REPO fuzzing
 $PYBIN fuzzing/bot.py --remote-host "$FUZZ_REMOTE_HOST" --basedir "$FUZZ_BASE_DIR"
