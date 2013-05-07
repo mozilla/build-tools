c918a7a046e780d1b8d9a37d332086fa06cf1cb1
Massimo Gervasini <mgervasini@mozilla.com>
2013-05-07 17:08:51 +0200

    Bug 851270 - follow up to fix cwd, r=catlee

--- lib/python/util/hg.py
+++ lib/python/util/hg.py
@@ -103,7 +103,7 @@ def purge(dest):
     """Purge the repository of all untracked and ignored files."""
     try:
         run_cmd(['hg', '--config', 'extensions.purge=', 'purge', '-a', '--all',
-                  dest], cwd=os.path.normpath(os.path.join(dest, '..')))
+                  dest], cwd=dest)
     except subprocess.CalledProcessError as e:
         log.debug('purge failed: %s' %e)
         raise
