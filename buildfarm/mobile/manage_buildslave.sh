f2b0c2a6c6ea5384fd581c57082edf02bb839358
Peter Moore <pmoore@mozilla.com>
2013-05-22 18:51:04 +0200

    Bug 869897: Cleanup of manage_buildslave.sh. r=Callek

--- buildfarm/mobile/manage_buildslave.sh
+++ buildfarm/mobile/manage_buildslave.sh
@@ -9,49 +9,54 @@ else
   BB_TWISTD=`which twistd`
 fi
 hash "$BB_PATH" "$BB_PYTHON" "$BB_TWISTD" 2>/dev/null || \
-    (echo "Can't find all needed executables to manage buildslave" 1>&2; exit 1)
+    (echo "Can't find all needed executables to manage buildslave" >&2; exit 1)
 
 OPTIONS="gettac start stop restart"
 
 opt="$1"
 
-if [ -z $opt ]; then
-    echo "You can call this script with the following options:"
-    echo $OPTIONS
-    exit 1
+if [ -z "$opt" ]; then
+    echo "You can call this script with the following options:" >&2
+    echo "$OPTIONS" >&2
+    exit 2
 else
     if [ -z $2 ]; then
-        echo You have to specify which device to manage.
-        exit 1
+        echo "You have to specify which device to manage." >&2
+        exit 2
     fi
-    DEVICE=$2
-    DEVICE_PATH=/builds/${DEVICE}
+    DEVICE="${2}"
+    DEVICE_PATH="/builds/${DEVICE}"
     if [ ! -d "$DEVICE_PATH" ]; then
-        echo "$DEVICE_PATH does not exist. Try again with the correct device name."
-        exit 1
+        echo "Directory '$DEVICE_PATH' does not exist. Try again with the correct device name." >&2
+        exit 64
     fi
 fi
 
-
-if [ "$opt" = "gettac" ]; then
-    rm $DEVICE_PATH/buildbot.tac
-    wget -q -O$DEVICE_PATH/buildbot.tac http://slavealloc.build.mozilla.org/gettac/$DEVICE
-elif [ "$opt" = "stop" ]; then
-    ${BB_PATH} stop $DEVICE_PATH
-elif [ "$opt" = "restart" ]; then
-    ${BB_PATH} stop $DEVICE_PATH
-    opt="start"
-fi
-
-if [ "$opt" = "start" ]; then
-    echo "We want to always start buildbot through twistd"
-    echo "We will run with the twistd command instead of calling buildslave"
-    pushd $DEVICE_PATH
-    ${BB_PYTHON} ${BB_TWISTD} --no_save \
-        --rundir=$DEVICE_PATH \
-        --pidfile=$DEVICE_PATH/twistd.pid \
-        --python=$DEVICE_PATH/buildbot.tac
-    sleep 1
-    tail $DEVICE_PATH/twistd.log
-    popd
-fi
+case "${opt}" in
+    gettac)
+        rm $DEVICE_PATH/buildbot.tac
+        wget -q "-O${DEVICE_PATH}/buildbot.tac" "http://slavealloc.build.mozilla.org/gettac/${DEVICE}"
+        ;;
+    stop)
+        "${BB_PATH}" stop "${DEVICE_PATH}"
+        ;;
+    restart)
+        "${BB_PATH}" stop "${DEVICE_PATH}"
+        # also perform 'start' actions - following line 'falls through' to start) section
+        # to achieve exactly this
+        ;&
+    start)
+        echo "We want to always start buildbot through twistd"
+        echo "We will run with the twistd command instead of calling buildslave"
+        pushd "${DEVICE_PATH}"
+        # should be in quotes in case we have a space in a parent directory name
+        "${BB_PYTHON}" "${BB_TWISTD}" --no_save \
+            "--rundir=$DEVICE_PATH" \
+            "--pidfile=$DEVICE_PATH/twistd.pid" \
+            "--python=$DEVICE_PATH/buildbot.tac"
+        sleep 1
+        tail twistd.log
+        popd
+        ;;
+    *)
+esac
