d89a33d49fa1d52c096a435a98e413c4a730d5d6
Ben Hearsum <bhearsum@mozilla.com>
2014-02-12 09:46:01 -0500

    Bump buildtools package version to pick up new reboot-idle-slaves.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="buildtools",
-    version="1.0.1",
+    version="1.0.2",
     description="Mozilla RelEng Toolkit",
     author="Release Engineers",
     author_email="release@mozilla.com",
