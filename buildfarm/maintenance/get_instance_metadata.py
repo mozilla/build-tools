87b248705ee69800ae94ed760c0f51342b449b2a
Chris AtLee <catlee@mozilla.com>
2014-03-14 13:56:45 -0400

    Bug 976415: Make sure we can read the file before reading it. r=Callek

--- buildfarm/maintenance/get_instance_metadata.py
+++ buildfarm/maintenance/get_instance_metadata.py
@@ -8,5 +8,5 @@ if sys.platform not in ('win32', 'cygwin'):
 else:
     path = r"C:\instance_metadata.json"
 
-if os.path.exists(path):
+if os.path.exists(path) and os.access(path, os.R_OK):
     print open(path).read()
