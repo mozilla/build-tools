31aa0792d4b32e1416dc1d1df697bd904acd50d7
Rail Aliiev <rail@mozilla.com>
2014-03-31 13:23:24 -0400

    Bug 916181 - Update firefox-latest, firefox-stub, firefox-latest-euballot bouncer aliases as a part of post-release builder (and same for beta). r=bhearsum

--- scripts/release/stage-tasks.py
+++ scripts/release/stage-tasks.py
@@ -265,7 +265,8 @@ def update_bouncer_alias(tuxedoServerUrl, auth, version,
 
     # Wrap the real call to hide credentials from retry's logging
     def do_update_bouncer_alias():
-        requests.post(url, data=data, auth=auth, config={'danger_mode': True})
+        requests.post(url, data=data, auth=auth, config={'danger_mode': True},
+                      verify=False)
 
     retry(do_update_bouncer_alias)
 
