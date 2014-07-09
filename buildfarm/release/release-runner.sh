ae16e6cbc9714657d31f653f44c5afd2ab48a6c5
Sylvestre Ledru <sylvestre@mozilla.com>
2014-07-09 16:48:20 +0200

    Bug 1036425 - Check the input arguments + Uses the "notify_to" value from the ini file instead of the hardcoded value. r=rail
    From d94c8e67445d46f81a6372f62a8e1376f0c97410 Mon Sep 17 00:00:00 2001
     "notify_to" value from the ini file instead of the hardcoded value. r=rail
    ---
     buildfarm/release/release-runner.sh | 24 +++++++++++++++++++++++-
     1 file changed, 23 insertions(+), 1 deletion(-)
    
    --HG--
    extra : amend_source : 63f3bd7389360646169f99a09c035a031559e5e3

--- buildfarm/release/release-runner.sh
+++ buildfarm/release/release-runner.sh
@@ -7,11 +7,29 @@ CONFIG=$3
 if [ -z "$VENV" ]; then
     VENV=/home/cltbld/release-runner/venv
 fi
+
+if [ ! -e "$VENV" ]; then
+    echo "Could not find Python virtual environment '$VENV'"
+    exit 1
+fi
+
 if [ -z "$LOGFILE" ]; then
     LOGFILE=/var/log/supervisor/release-runner.log
 fi
+
+LOGFILE_DIR=$(dirname $LOGFILE)
+if [ ! -e $LOGFILE_DIR  ]; then
+    echo "Could not find directory '$LOGFILE_DIR' for the logs"
+    exit 1
+fi
+
 if [ -z "$CONFIG" ]; then
     CONFIG=/home/cltbld/.release-runner.ini
+fi
+
+if [ ! -e "$CONFIG" ]; then
+    echo "Could not find configuration file '$CONFIG'"
+    exit 1
 fi
 
 # Mozilla hg is symlinked as /usr/local/bin/hg
@@ -21,7 +39,11 @@ export PATH=/usr/local/bin:$PATH
 
 # Sleep time after a failure, in seconds.
 SLEEP_TIME=60
-NOTIFY_TO=release@mozilla.com
+NOTIFY_TO=$(grep "notify_to:" $CONFIG|sed -e "s|.*<\(.*\)>|\1|g")
+if [ -z "$NOTIFY_TO" ]; then
+    NOTIFY_TO="release@mozilla.com"
+fi
+
 
 CURR_DIR=$(cd $(dirname $0); pwd)
 HOSTNAME=`hostname -s`
