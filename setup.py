8340dbd62edb5f558f0db1e21187bbf207af69d9
Justin Wood <Callek@gmail.com>
2014-08-15 12:28:22 -0400

    Bug 1054350 - Deploy newer slaverebooter. r=coop

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="buildtools",
-    version="1.0.4",
+    version="1.0.5",
     description="Mozilla RelEng Toolkit",
     author="Release Engineers",
     author_email="release@mozilla.com",
