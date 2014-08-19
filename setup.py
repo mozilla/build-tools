884a07b55243904cdf2ad0fca2ba0af5f7815520
Justin Wood <Callek@gmail.com>
2014-08-18 23:50:49 -0400

    Bug 1055392 - Deploy newer slaverebooter. stamp r=nthomas

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="buildtools",
-    version="1.0.5",
+    version="1.0.6",
     description="Mozilla RelEng Toolkit",
     author="Release Engineers",
     author_email="release@mozilla.com",
