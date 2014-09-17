2057f55f69a906d474b668ac492a2d2ecf9a0246
John Zeller <jozeller@mozilla.com>
2014-09-17 13:28:07 -0700

    Bug 1055191 - deploy ship it notifier - fixing path insert and removing ssl from pulse consumer for shipit_notifier.py, r=bhearsum

--- buildfarm/release/shipit-notifier.py
+++ buildfarm/release/shipit-notifier.py
@@ -14,11 +14,12 @@ from ConfigParser import ConfigParser
 from release.info import getReleaseName
 from mozillapulse import consumers
 from mozillapulse import config as pconf
-from kickoff.api import Status
 from os import path
 from dateutil import parser
 
-site.addsitedir(path.join(path.dirname(__file__), "../../lib/python"))
+import sys
+sys.path.insert(0, path.join(path.dirname(__file__), "../../lib/python"))
+from kickoff.api import Status
 
 import logging as log
 
@@ -116,7 +117,7 @@ def main():
     )
 
     # Adjust applabel when wanting to run shipit on multiple machines
-    pulse = consumers.BuildConsumer(applabel='shipit-notifier')
+    pulse = consumers.BuildConsumer(applabel='shipit-notifier', ssl=False)
     pulse.configure(topic='build.#.finished',
                     durable=True, callback=got_message)
 
