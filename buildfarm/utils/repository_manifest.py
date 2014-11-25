5543ff25091b750e77e4c3a4c796ba29fd5a027b
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2014-11-25 11:33:14 -0500

    Bug 791924 - Switch repository_manifest.py to use optparse instead of argparse. r=rail

--- buildfarm/utils/repository_manifest.py
+++ buildfarm/utils/repository_manifest.py
@@ -8,13 +8,15 @@
 # Author(s):     Zambrano Gasparnian, Armen <armenzg@mozilla.com>
 # Target:        Python 2.7.x
 #
-"""Reads a repository manifest and outputs the repo and
-revision/branch in a format digestable for buildbot
-properties: "key: value"
 """
-import argparse
+   Reads a repository manifest and outputs the repo and
+   revision/branch in a format digestable for buildbot
+   properties ("key: value").
+"""
 import json
 import urllib2
+
+from optparse import OptionParser
 
 # When an infra error happens we want to turn purple and
 # let sheriffs determine if re-triggering is needed
@@ -25,16 +27,20 @@ def main():
     Determine which repository and revision mozharness.json indicates.
     If none is found we fall back to the default repository
     '''
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--manifest-url", dest="manifest_url", required=True)
-    parser.add_argument("--default-repo", dest="default_repo", required=True)
-    parser.add_argument("--default-revision", dest="default_revision",
-                        required=True)
-    args = parser.parse_args()
+    parser = OptionParser(__doc__)
+    parser.add_option("--manifest-url", dest="manifest_url")
+    parser.add_option("--default-repo", dest="default_repo")
+    parser.add_option("--default-revision", dest="default_revision")
+    options, args = parser.parse_args()
+
+    if not options.manifest_url or \
+       not options.default_repo or \
+       not options.default_revision:
+        parser.error("You have to call the script with all options")
 
     exit_code = 0
     try:
-        url_opener = urllib2.urlopen(args.manifest_url, timeout=10)
+        url_opener = urllib2.urlopen(options.manifest_url, timeout=10)
         http_code = url_opener.getcode()
         if http_code == 200:
             manifest = json.load(url_opener)
@@ -48,8 +54,8 @@ def main():
     except urllib2.HTTPError, e:
         # Fallback to default values for branches where the manifest
         # is not defined
-        print "script_repo_url: %s" % args.default_repo
-        print "script_repo_revision: %s" % args.default_revision
+        print "script_repo_url: %s" % options.default_repo
+        print "script_repo_revision: %s" % options.default_revision
         exit_code = 0
     except Exception, e:
         print str(e)
