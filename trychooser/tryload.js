a035bd389a4e8a12755b6b048d63271ee526048a
Jordan Lund <jlund@mozilla.com>
2014-12-16 11:40:24 -0800

    Bug 1108058 - add trychooser support for new android builds, api 11 support, r=philor

--- trychooser/tryload.js
+++ trychooser/tryload.js
@@ -64,7 +64,7 @@ function getTryLoads(callback) {
         totalBuildLoad["win32"] = load_try_build["win64"];
         totalBuildLoad["win64"] = totalBuildLoad["win32"];
         totalBuildLoad["android-api-9"] = totalBuildLoad["linux"];
-        totalBuildLoad["android-api-10"] = totalBuildLoad["linux"];
+        totalBuildLoad["android-api-11"] = totalBuildLoad["linux"];
         totalBuildLoad["android-x86"] = totalBuildLoad["linux"];
         totalBuildLoad["emulator"] = totalBuildLoad["linux"];
         totalBuildLoad["emulator-jb"] = totalBuildLoad["linux"];
@@ -92,7 +92,7 @@ function getTryLoads(callback) {
         totalTestLoad["win64"] = load_try_test["win2012x64"] + load_nontry_test["win2012x64"];
         totalTestLoad["android-api-9"] = Math.max(
                                     load_try_test["ubuntu64-vm"] + load_nontry_test["ubuntu64-vm"]);
-        totalTestLoad["android-api-10"] = Math.max(
+        totalTestLoad["android-api-11"] = Math.max(
                                     load_try_test["panda"] + load_nontry_test["panda"]);
         totalTestLoad["android-x86"] = load_try_test["ubuntu64-hw"]
         totalTestLoad["emulator"] = load_try_test["ubuntu64-vm"]
