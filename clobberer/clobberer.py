b2fc1e4a64077b559f709fd0d61461bd859f1468
Morgan Phillips <winter2718@gmail.com>
2015-02-27 17:54:18 +0000

    Bug 1131790 - Give the clobberer client the ability to do bulk clobbers; r=rail
    This adds support for a new json endpoint which returns all recent clobbers
    and maintains support for the old endpoint by translating it's output
    into a json response as well.
    
    In this version, argopts has been used in favor of argparse (for python2.6 compatibility)
    
    --HG--
    extra : amend_source : 16eb3123b6272b95a1d259392c5487f8b573c8d7

--- clobberer/clobberer.py
+++ clobberer/clobberer.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
-# vim:sts=2 sw=2
+# vim:sts=4 sw=4
 import sys
 import shutil
 import urllib2
 import urllib
+import json
 import os
-import traceback
 import time
+import socket
+import logging
+
+log = logging.getLogger()
+
 if os.name == 'nt':
     from win32file import RemoveDirectory, DeleteFile, \
-        GetFileAttributesW, SetFileAttributesW, \
+        SetFileAttributesW, \
         FILE_ATTRIBUTE_NORMAL, FILE_ATTRIBUTE_DIRECTORY
     from win32api import FindFiles
 
 clobber_suffix = '.deleteme'
+
+
+def get_hostname():
+    return socket.gethostname().split('.')[0]
 
 
 def ts_to_str(ts):
@@ -39,8 +48,24 @@ def read_file(fn):
     except ValueError:
         return None
 
-def rmdirRecursiveWindows(dir):
-    """Windows-specific version of rmdirRecursive that handles
+
+def safe_join(parent, path):
+    """Returns $parent/$path
+    Raises IOError in case the resulting path ends up outside of parent for
+    some reason (e.g. by using ../../..)
+    """
+    while os.path.isabs(path):
+        path = path.lstrip("/")
+        drive, tail = os.path.splitdrive(path)
+        path = tail
+        path = os.path.normpath(path)
+    if path.startswith("../"):
+        raise IOError("unsafe join of %s/%s" % (parent, path))
+    return os.path.join(parent, path)
+
+
+def rmdir_recursive_windows(dir):
+    """Windows-specific version of rmdir_recursive that handles
     path lengths longer than MAX_PATH.
     """
 
@@ -56,18 +81,19 @@ def rmdirRecursiveWindows(dir):
         full_name = os.path.join(dir, name)
 
         if file_attr & FILE_ATTRIBUTE_DIRECTORY:
-            rmdirRecursiveWindows(full_name)
+            rmdir_recursive_windows(full_name)
         else:
             SetFileAttributesW('\\\\?\\' + full_name, FILE_ATTRIBUTE_NORMAL)
             DeleteFile('\\\\?\\' + full_name)
     RemoveDirectory('\\\\?\\' + dir)
 
-def rmdirRecursive(dir):
+
+def rmdir_recursive(dir):
     """This is a replacement for shutil.rmtree that works better under
     windows. Thanks to Bear at the OSAF for the code.
     (Borrowed from buildbot.slave.commands)"""
     if os.name == 'nt':
-        rmdirRecursiveWindows(dir)
+        rmdir_recursive_windows(dir)
         return
 
     if not os.path.exists(dir):
@@ -81,7 +107,7 @@ def rmdirRecursive(dir):
         return
 
     # Verify the directory is read/write/execute for the current user
-    os.chmod(dir, 0700)
+    os.chmod(dir, 0o700)
 
     for name in os.listdir(dir):
         full_name = os.path.join(dir, name)
@@ -92,14 +118,14 @@ def rmdirRecursive(dir):
                 # I think this is now redundant, but I don't have an NT
                 # machine to test on, so I'm going to leave it in place
                 # -warner
-                os.chmod(full_name, 0600)
+                os.chmod(full_name, 0o600)
 
         if os.path.isdir(full_name):
-            rmdirRecursive(full_name)
+            rmdir_recursive(full_name)
         else:
             # Don't try to chmod links
             if not os.path.islink(full_name):
-                os.chmod(full_name, 0700)
+                os.chmod(full_name, 0o700)
             os.remove(full_name)
     os.rmdir(dir)
 
@@ -108,11 +134,11 @@ def do_clobber(dir, dryrun=False, skip=None):
     try:
         for f in os.listdir(dir):
             if skip is not None and f in skip:
-                print "Skipping", f
+                log.info("Skipping %s", f)
                 continue
             clobber_path = f + clobber_suffix
             if os.path.isfile(f):
-                print "Removing", f
+                log.info("Removing %s", f)
                 if not dryrun:
                     if os.path.exists(clobber_path):
                         os.unlink(clobber_path)
@@ -123,26 +149,31 @@ def do_clobber(dir, dryrun=False, skip=None):
                         shutil.move(f, clobber_path)
                         os.unlink(clobber_path)
             elif os.path.isdir(f):
-                print "Removing %s/" % f
+                log.info("Removing %s/", f)
                 if not dryrun:
                     if os.path.exists(clobber_path):
-                        rmdirRecursive(clobber_path)
+                        rmdir_recursive(clobber_path)
                     # Prevent repeated moving.
                     if f.endswith(clobber_suffix):
-                        rmdirRecursive(f)
+                        rmdir_recursive(f)
                     else:
                         shutil.move(f, clobber_path)
-                        rmdirRecursive(clobber_path)
+                        rmdir_recursive(clobber_path)
     except Exception as e:
-        print("[clobber failed]: {}".format(e.message))
+        log.error("[clobber failed]: %s", e.message)
         sys.exit(1)
 
 
-def getClobberDates(clobberURL, branch, buildername, builddir, slave, master):
+def get_clobber_times(clobber_url):
+    log.info("Checking clobber URL: %s", clobber_url)
+    data = urllib2.urlopen(clobber_url, timeout=30).read()
+    return json.loads(data)
+
+
+def legacy_get_clobber_times(clobber_url, branch, buildername, builddir, slave, master=None):
     params = dict(branch=branch, buildername=buildername,
                   builddir=builddir, slave=slave, master=master)
-    url = "%s?%s" % (clobberURL, urllib.urlencode(params))
-    print "Checking clobber URL: %s" % url
+    url = "%s?%s" % (clobber_url, urllib.urlencode(params))
     # The timeout arg was added to urlopen() at Python 2.6
     # Deprecate this test when esr17 reaches EOL
     if sys.version_info[:2] < (2, 6):
@@ -150,131 +181,148 @@ def getClobberDates(clobberURL, branch, buildername, builddir, slave, master):
     else:
         data = urllib2.urlopen(url, timeout=30).read().strip()
 
-    retval = {}
-    try:
-        for line in data.split("\n"):
-            line = line.strip()
-            if not line:
-                continue
-            builddir, builder_time, who = line.split(":")
-            builder_time = int(builder_time)
-            retval[builddir] = (builder_time, who)
-        return retval
-    except ValueError:
-        print "Error parsing response from server"
-        print data
-        raise
+    processed_data = data.strip().split(':')
 
-if __name__ == "__main__":
-    from optparse import OptionParser
-    parser = OptionParser(
-        "%prog [options] clobberURL branch buildername builddir slave master")
-    parser.add_option("-n", "--dry-run", dest="dryrun", action="store_true",
-                      default=False, help="don't actually delete anything")
-    parser.add_option("-t", "--periodic", dest="period", type="float",
-                      default=None, help="hours between periodic clobbers")
-    parser.add_option('-s', '--skip', help='do not delete this file/directory',
-                      action='append', dest='skip', default=['last-clobber'])
-    parser.add_option('-d', '--dir', help='clobber this directory',
-                      dest='dir', default='.', type='string')
-    parser.add_option('-v', '--verbose', help='be more verbose',
-                      dest='verbose', action='store_true', default=False)
+    if len(processed_data) < 2:
+        # We received an empty result, time to bail
+        return {'result': []}
 
-    options, args = parser.parse_args()
-    if len(args) != 6:
-        parser.error("Incorrect number of arguments")
+    builddir_result, lastclobber, who = processed_data
+    lastclobber = int(lastclobber)
+    # return a dictionary that mimicks the new endpoint
+    # json format
+    return {
+        'result': [{
+            'builddir': builddir_result,
+            'lastclobber': lastclobber,
+            'who': who,
+            'slave': None
+        }]
+    }
 
-    if options.period:
-        periodicClobberTime = options.period * 3600
-    else:
-        periodicClobberTime = None
 
-    clobberURL, branch, builder, my_builddir, slave, master = args
-
-    try:
-        server_clobber_dates = getClobberDates(
-            clobberURL, branch, builder, my_builddir, slave, master)
-    except:
-        if options.verbose:
-            traceback.print_exc()
-        print "Error contacting server"
-        sys.exit(1)
-
-    if options.verbose:
-        print "Server gave us", server_clobber_dates
-
+def process_clobber_times(server_clobber_times, args):
     now = int(time.time())
 
-    # Add ourself to the server_clobber_dates if it's not set
-    # This happens when this slave has never been clobbered
-    if my_builddir not in server_clobber_dates:
-        server_clobber_dates[my_builddir] = None, ""
+    root_dir = os.path.abspath(args.dir)
 
-    root_dir = os.path.abspath(options.dir)
+    for clobber_time in server_clobber_times['result']:
+        slave = clobber_time['slave']
+        builddir = clobber_time['builddir']
+        server_clobber_date = clobber_time['lastclobber']
+        who = clobber_time['who']
 
-    for builddir, (server_clobber_date, who) in server_clobber_dates.items():
-        builder_dir = os.path.join(root_dir, builddir)
+        builder_dir = safe_join(root_dir, builddir)
         if not os.path.isdir(builder_dir):
-            print "%s doesn't exist, skipping" % builder_dir
+            log.info("%s doesn't exist, skipping", builder_dir)
             continue
         os.chdir(builder_dir)
 
-        our_clobber_date = read_file("last-clobber")
+        our_clobber_date = read_file("last-clobber") or 0
 
         clobber = False
-        clobberType = None
+        clobber_type = None
 
-        print "%s:Our last clobber date: " % builddir, ts_to_str(our_clobber_date)
-        print "%s:Server clobber date:   " % builddir, ts_to_str(server_clobber_date)
+        log.info("%s:Our last clobber date: %s", builddir, ts_to_str(our_clobber_date))
+        log.info("%s:Server clobber date:   %s", builddir, ts_to_str(server_clobber_date))
 
         # If we don't have a last clobber date, then this is probably a fresh build.
         # We should only do a forced server clobber if we know when our last clobber
         # was, and if the server date is more recent than that.
         if server_clobber_date is not None and our_clobber_date is not None:
             # If the server is giving us a clobber date, compare the server's idea of
-            # the clobber date to our last clobber date
-            if server_clobber_date > our_clobber_date:
+            # the clobber date to our last clobber date also skip slave specific
+            # clobbers that aren't for the current host
+            if (server_clobber_date > our_clobber_date) and (slave is None or slave == args.slave):
                 # If the server's clobber date is greater than our last clobber date,
                 # then we should clobber.
                 clobber = True
-                clobberType = "forced"
+                clobber_type = "forced"
                 # We should also update our clobber date to match the server's
                 our_clobber_date = server_clobber_date
                 if who:
-                    print "%s:Server is forcing a clobber, initiated by %s" % (builddir, who)
+                    log.info("%s:Server is forcing a clobber, initiated by %s", builddir, who)
                 else:
-                    print "%s:Server is forcing a clobber" % builddir
+                    log.info("%s:Server is forcing a clobber", builddir)
 
         if not clobber:
-            # Disable periodic clobbers for builders that aren't my_builddir
-            if builddir != my_builddir:
+            # Disable periodic clobbers for builders that aren't args.builddir
+            if builddir != args.builddir:
                 continue
 
-            # Next, check if more than the periodicClobberTime period has passed since
+            # Next, check if more than the periodic_clobber_time period has passed since
             # our last clobber
             if our_clobber_date is None:
                 # We've never been clobbered
                 # Set our last clobber time to now, so that we'll clobber
-                # properly after periodicClobberTime
-                clobberType = "purged"
+                # properly after periodic_clobber_time
+                clobber_type = "purged"
                 our_clobber_date = now
                 write_file(our_clobber_date, "last-clobber")
-            elif periodicClobberTime and now > our_clobber_date + periodicClobberTime:
-                # periodicClobberTime has passed since our last clobber
+            elif args.periodic_clobber_time and now > our_clobber_date + args.periodic_clobber_time:
+                # periodic_clobber_time has passed since our last clobber
                 clobber = True
-                clobberType = "periodic"
+                clobber_type = "periodic"
                 # Update our clobber date to now
                 our_clobber_date = now
-                print "%s:More than %s seconds have passed since our last clobber" % (builddir, periodicClobberTime)
+                log.info("%s:More than %s seconds have passed since our last clobber",
+                         (builddir, args.periodic_clobber_time))
 
         if clobber:
             # Finally, perform a clobber if we're supposed to
-            print "%s:Clobbering..." % builddir
-            do_clobber(builder_dir, options.dryrun, options.skip)
+            log.info("%s:Clobbering...", builddir)
+            do_clobber(builder_dir, args.dryrun, args.skip)
             write_file(our_clobber_date, "last-clobber")
 
         # If this is the build dir for the current job, display the clobber type in TBPL.
         # Note in the case of purged clobber, we output the clobber type even though no
         # clobber was performed this time.
-        if clobberType and builddir == my_builddir:
-            print "TinderboxPrint: %s clobber" % clobberType
+        if clobber_type and builddir == args.builddir:
+            log.info("Tinderboxlog.info: %s clobber" % clobber_type)
+
+
+def make_argparser():
+    import optparse
+    parser = optparse.OptionParser(
+        "%prog [options] clobberURL branch buildername builddir slave master"
+    )
+    parser.add_option("-n", "--dry-run", dest="dryrun", action="store_true",
+                      default=False, help="don't actually delete anything")
+    parser.add_option("-t", "--periodic", dest="period", type=float,
+                      default=None, help="hours between periodic clobbers")
+    parser.add_option('-s', '--skip', help='do not delete this file/directory',
+                      action='append', dest='skip', default=['last-clobber'])
+    parser.add_option('-d', '--dir', help='clobber this directory',
+                      dest='dir', default='.')
+    parser.add_option('--builddir', help='a specific builddir for periodic clobbers')
+    parser.add_option('-v', '--verbose', help='be more verbose',
+                      dest='loglevel', action='store_const',
+                      default=logging.INFO, const=logging.DEBUG)
+    parser.add_option('--url', dest='clobber_url', help='URL to clobberer service')
+    parser.add_option('--slave', default=get_hostname(),
+                      help='Name of the slave being clobbered (defaults to hostname)')
+    return parser
+
+
+def main(args, legacy=None):
+    if args.period:
+        args.periodic_clobber_time = args.period * 3600
+    else:
+        args.periodic_clobber_time = None
+    # Legacy support
+    if legacy and len(legacy) > 1:
+        log.info("Entering legacy mode.")
+        server_clobber_times = legacy_get_clobber_times(*legacy)
+    elif not args.clobber_url:
+        # we can't set required=True because of legacy support
+        raise Exception("--url required")
+    else:
+        server_clobber_times = get_clobber_times(args.clobber_url)
+
+    process_clobber_times(server_clobber_times, args)
+
+if __name__ == "__main__":
+    parser = make_argparser()
+    parsed_args, legacy = parser.parse_args()
+    logging.basicConfig(level=parsed_args.loglevel, format="%(asctime)s - %(message)s")
+    main(parsed_args, legacy=legacy)
--- clobberer/test_clobberer.py
+++ clobberer/test_clobberer.py
@@ -1,10 +1,17 @@
 from unittest import TestCase
 import tempfile
 import shutil
+import json
+import mock
 import os
 
 from clobberer import do_clobber
-from clobberer import getClobberDates
+from clobberer import (
+    get_clobber_times,
+    legacy_get_clobber_times,
+    make_argparser,
+    process_clobber_times
+)
 
 
 class TestClobbererClient(TestCase):
@@ -12,8 +19,27 @@ class TestClobbererClient(TestCase):
         self.outer_dir = tempfile.mkdtemp()
         os.chdir(self.outer_dir)  # replicating client logic
 
+        self.slave_name = 'un-petit-bebe'
+        parser = make_argparser()
+        self.process_args, legacy = parser.parse_args([
+            '--dir',
+            '/',
+            '--slave',
+            self.slave_name
+        ])
+
     def tearDown(self):
         shutil.rmtree(self.outer_dir)
+
+    def gen_server_clobber_times(self, slave_name=None):
+        return {'result': [
+            {
+                'builddir': 'tmp',
+                'lastclobber': 1,
+                'who': 'albert.camus@gmail.net',
+                'slave': slave_name
+            }
+        ]}
 
     def test_do_clobber(self):
         file_name = 'my-name-is-mud'
@@ -43,27 +69,53 @@ class TestClobbererClient(TestCase):
         os.rmdir(skip_dir_name)
         os.remove(skip_file_name)
 
-    def test_get_clobber_dates(self):
-        import urllib2
+    @mock.patch('urllib2.urlopen')
+    def test_get_clobber_times(self, mock_urlopen):
+        builddir = 'the-roadhouse'
+        timestamp = 9999
+        who = 'JimMorrison@thedoors.net'
+        fake_data = {'result': [
+            {
+                'builddir': builddir,
+                'lastclobber': timestamp,
+                'who': who,
+                'slave': None
+                }
+            ]
+        }
+        fake_data_json_str = json.dumps(fake_data)
+        urllib2_mock = mock.Mock()
+        urllib2_mock.read.side_effect = [fake_data_json_str]
+        mock_urlopen.return_value = urllib2_mock
+
+        clobber_dates_return = get_clobber_times('clobberer/lastclobber/all')
+        self.assertDictEqual(clobber_dates_return, fake_data)
+
+    @mock.patch('urllib2.urlopen')
+    def test_get_clobber_times_null(self, mock_urlopen):
+        # make sure it can handle no data
+        fake_data_null = {'result': []}
+        fake_data_json_str = json.dumps(fake_data_null)
+        urllib2_mock = mock.Mock()
+        urllib2_mock.read.side_effect = [fake_data_json_str]
+        mock_urlopen.return_value = urllib2_mock
+
+        clobber_dates_return = get_clobber_times('clobberer/lastclobber/all')
+        self.assertDictEqual(clobber_dates_return, fake_data_null)
+
+    @mock.patch('urllib2.urlopen')
+    def test_legacy_get_clobber_times(self, mock_urlopen):
         lastclobber_fmt = '{}:{}:{}\n'
         builddir = 'the-roadhouse'
         timestamp = 9999
         who = 'JimMorrison@thedoors.net'
-        FAKE_URLOPEN_DATA = lastclobber_fmt.format(builddir, timestamp, who)
 
-        class FakeURLOpen(object):
+        fake_data_str = lastclobber_fmt.format(builddir, timestamp, who)
+        urllib2_mock = mock.Mock()
+        urllib2_mock.read.side_effect = [fake_data_str]
+        mock_urlopen.return_value = urllib2_mock
 
-            def __init__(self, *args, **kwargs):
-                pass
-
-            def read(self):
-                # some fake clobber data
-                return FAKE_URLOPEN_DATA
-
-        # monkey patch urllib2 to force simulated clobber data
-        urllib2.urlopen = FakeURLOpen
-
-        clobber_dates_return = getClobberDates(
+        clobber_dates_return = legacy_get_clobber_times(
             'clobberer/lastclobber',
             'branch',
             'buildername',
@@ -71,11 +123,28 @@ class TestClobbererClient(TestCase):
             'slave',
             'master',
         )
-        self.assertEqual(clobber_dates_return, {builddir: (timestamp, who)})
 
+        self.assertDictEqual(
+            clobber_dates_return,
+            {
+                'result': [{
+                    'builddir': builddir,
+                    'lastclobber': timestamp,
+                    'who': who,
+                    'slave': None
+                }]
+            }
+        )
+
+    @mock.patch('urllib2.urlopen')
+    def test_legacy_get_clobber_times_null(self, mock_urlopen):
         # make sure it can handle no data
-        FAKE_URLOPEN_DATA = ""
-        clobber_dates_return = getClobberDates(
+        fake_data_str = ""
+        urllib2_mock = mock.Mock()
+        urllib2_mock.read.side_effect = [fake_data_str]
+        mock_urlopen.return_value = urllib2_mock
+
+        clobber_dates_return = legacy_get_clobber_times(
             'clobberer/lastclobber',
             'branch',
             'buildername',
@@ -83,7 +152,30 @@ class TestClobbererClient(TestCase):
             'slave',
             'master',
         )
-        self.assertEqual(clobber_dates_return, {})
+        self.assertDictEqual(clobber_dates_return, {'result': []})
+
+    @mock.patch('clobberer.write_file')
+    @mock.patch('clobberer.do_clobber')
+    def test_process_clobber_times_no_slave(self, mock_do_clobber, mock_write_file):
+        server_clobber_times_no_slave = self.gen_server_clobber_times()
+        # so we can tell if a clobber has been triggered
+        process_clobber_times(server_clobber_times_no_slave, self.process_args)
+        mock_do_clobber.assert_called_with('/tmp', False, ['last-clobber'])
+
+    @mock.patch('clobberer.write_file')
+    @mock.patch('clobberer.do_clobber')
+    def test_process_clobber_times_wrong_slave(self, mock_do_clobber, mock_write_file):
+        # should cause no response
+        server_clobber_times_wrong_slave = self.gen_server_clobber_times('iamamwhoami')
+        process_clobber_times(server_clobber_times_wrong_slave, self.process_args)
+        assert not mock_do_clobber.called
+
+    @mock.patch('clobberer.write_file')
+    @mock.patch('clobberer.do_clobber')
+    def test_process_clobber_times_right_slave(self, mock_do_clobber, mock_write_file):
+        server_clobber_times_right_slave = self.gen_server_clobber_times(self.slave_name)
+        process_clobber_times(server_clobber_times_right_slave, self.process_args)
+        mock_do_clobber.assert_called_with('/tmp', False, ['last-clobber'])
 
 if __name__ == '__main__':
     import unittest
