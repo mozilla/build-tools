2d3a4adbbda0d08c47ee2a33a561e8f7c52b3e34
kmoir@mozilla.com <kmoir@mozilla.com>
2015-04-07 15:49:38 -0400

    Bug 1131269 - use SETA data to disable unneeded tests r=Callek

--- lib/python/util/fabric/actions.py
+++ lib/python/util/fabric/actions.py
@@ -243,6 +243,10 @@ def action_add_gecko_version_symlinks(master):
         run('ln -s %(bbconfigs_dir)s/mozilla/gecko_versions.json '
             '%(master_dir)s/' % master)
 
+def action_add_config_seta_symlinks(master):
+    with show('running'):
+        run('ln -s %(bbconfigs_dir)s/mozilla-tests/config_seta.py '
+            '%(master_dir)s/' % master)
 
 def action_update_exception_timestamp(master):
     with show('running'):
