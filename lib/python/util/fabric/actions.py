21e4260209bf1657e575de585df674fe48fcbc00
kmoir@mozilla.com <kmoir@mozilla.com>
2015-04-23 11:45:12 -0400

    Bug 1155362 - enable SETA for Android in buildbot scheduling, configs r=Callek

--- lib/python/util/fabric/actions.py
+++ lib/python/util/fabric/actions.py
@@ -243,10 +243,18 @@ def action_add_gecko_version_symlinks(master):
         run('ln -s %(bbconfigs_dir)s/mozilla/gecko_versions.json '
             '%(master_dir)s/' % master)
 
+
 def action_add_config_seta_symlinks(master):
     with show('running'):
         run('ln -s %(bbconfigs_dir)s/mozilla-tests/config_seta.py '
             '%(master_dir)s/' % master)
+
+
+def action_add_config_seta_mobile_symlinks(master):
+    with show('running'):
+        run('ln -s %(bbconfigs_dir)s/mozilla-tests/config_seta_mobile.py '
+            '%(master_dir)s/' % master)        
+
 
 def action_update_exception_timestamp(master):
     with show('running'):
