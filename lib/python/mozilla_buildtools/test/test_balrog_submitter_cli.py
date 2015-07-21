2742aa671abff03297c8627517e9379e6ce247e6
Ben Hearsum <bhearsum@mozilla.com>
2015-07-21 12:49:48 -0400

    Backout balrog submitter changes to fix submitter.

--- lib/python/mozilla_buildtools/test/test_balrog_submitter_cli.py
+++ lib/python/mozilla_buildtools/test/test_balrog_submitter_cli.py
@@ -1,10 +1,7 @@
-try:
-    # Python 2.6 backport with assertDictEqual()
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+import unittest
+import sys
 from balrog.submitter.cli import NightlySubmitterBase, NightlySubmitterV4
-from balrog.submitter.updates import merge_partial_updates
+
 
 class TestNightlySubmitterBase(unittest.TestCase):
 
@@ -24,6 +21,8 @@ class TestNightlySubmitterBase(unittest.TestCase):
 class TestNightlySubmitterV4(unittest.TestCase):
 
     def test_canonical_ur_replacement(self):
+        if sys.version_info < (2, 7):
+            return
         url_replacements = [
             ("ftp.mozilla.org", "download.cdn.mozilla.net")
         ]
@@ -45,6 +44,8 @@ class TestNightlySubmitterV4(unittest.TestCase):
             }]})
 
     def test_no_canonical_ur_replacement(self):
+        if sys.version_info < (2, 7):
+            return
         submitter = NightlySubmitterV4(api_root=None, auth=None,
                                        url_replacements=None)
         completeInfo = [{
@@ -61,96 +62,3 @@ class TestNightlySubmitterV4(unittest.TestCase):
                 'from': '*',
                 'hashValue': 'abcd'
             }]})
-
-
-class TestUpdateMerger(unittest.TestCase):
-    # print the diff between large dicts
-    maxDiff = None
-
-    def test_merge_updates(self):
-        old_data = {
-            'some_other_field': "123",
-            'some_other_field2': {"a": "b", "c": 1},
-            'some_other_list': [1, 2, 3],
-            'completes': [
-                {
-                    'fileUrl': 'https://complete1',
-                    'filesize': 123,
-                    'from': '*',
-                    'hashValue': '123abcdef'
-                },
-            ],
-            'partials': [
-                {
-                    'fileUrl': 'https://partial1',
-                    'filesize': 111,
-                    'from': '111',
-                    'hashValue': '123abc'
-                },
-                {
-                    'fileUrl': 'https://partial2',
-                    'filesize': 112,
-                    'from': '112',
-                    'hashValue': '223abc'
-                },
-            ]
-        }
-        new_data = {
-            'completes': [
-                {
-                    'fileUrl': 'https://complete2',
-                    'filesize': 122,
-                    'from': '*',
-                    'hashValue': '122abcdef'
-                },
-            ],
-            'partials': [
-                {
-                    'fileUrl': 'https://partial2/differenturl',
-                    'filesize': 112,
-                    'from': '112',
-                    'hashValue': '223abcd'
-                },
-                {
-                    'fileUrl': 'https://partial3',
-                    'filesize': 113,
-                    'from': '113',
-                    'hashValue': '323abc'
-                },
-            ]
-        }
-        merged = merge_partial_updates(old_data, new_data)
-        expected_merged = {
-            'some_other_field': "123",
-            'some_other_field2': {"a": "b", "c": 1},
-            'some_other_list': [1, 2, 3],
-            'completes': [
-                {
-                    'fileUrl': 'https://complete2',
-                    'filesize': 122,
-                    'from': '*',
-                    'hashValue': '122abcdef'
-                },
-            ],
-            'partials': [
-                {
-                    'fileUrl': 'https://partial1',
-                    'filesize': 111,
-                    'from': '111',
-                    'hashValue': '123abc'
-                },
-                {
-                    'fileUrl': 'https://partial2/differenturl',
-                    'filesize': 112,
-                    'from': '112',
-                    'hashValue': '223abcd'
-                },
-                {
-                    'fileUrl': 'https://partial3',
-                    'filesize': 113,
-                    'from': '113',
-                    'hashValue': '323abc'
-                },
-            ]
-        }
-        self.assertDictEqual(merged, expected_merged)
