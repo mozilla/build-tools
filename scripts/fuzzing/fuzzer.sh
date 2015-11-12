6e9c891a87e595254d7a710e3591f80cef4862c9
Gary Kwong <gary@rumblingedge.com>
2015-11-12 12:47:30 -0800

    Clone FuzzManager repository in addition to funfuzz and others. r=coop

--- scripts/fuzzing/fuzzer.sh
+++ scripts/fuzzing/fuzzer.sh
@@ -31,6 +31,10 @@ if [ "$GIT_FUNFUZZ_PRIVATE_REPO" == "" ]; then
     echo "GIT_FUNFUZZ_PRIVATE_REPO not set."
 exit 13
 fi
+if [ "$GIT_FUZZMANAGER_REPO" == "" ]; then
+    echo "GIT_FUZZMANAGER_REPO not set."
+exit 14
+fi
 REPO_NAME="funfuzz"
 # We need to wrap our git operation for private repos in a shell script so
 # we can set things like the user (gitolite3). We use GIT_PRIVATE_SSH to
@@ -41,6 +45,7 @@ chmod 755 $GIT_PRIVATE_SSH
 rm -rf $REPO_NAME lithium funfuzz-private
 $GITBIN clone $GIT_FUNFUZZ_REPO $REPO_NAME
 $GITBIN clone $GIT_LITHIUM_REPO lithium
+$GITBIN clone $GIT_FUZZMANAGER_REPO FuzzManager
 GIT_SSH=$GIT_PRIVATE_SSH $GITBIN clone $GIT_FUNFUZZ_PRIVATE_REPO funfuzz-private
 
 $PYBIN $REPO_NAME/bot.py --remote-host "$FUZZ_REMOTE_HOST" --basedir "$FUZZ_BASE_DIR"
