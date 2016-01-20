e9dded4d2bb360be3d5c28db902864c767ded61e
Jordan Lund <jlund@mozilla.com>
2016-01-19 16:05:19 -0800

    Bug 1225899 - add beet mover tasks to releasetasks task graph, r=rail

--- buildfarm/release/release-runner.py
+++ buildfarm/release/release-runner.py
@@ -16,6 +16,7 @@ from kickoff.api import Releases, Release, ReleaseL10n
 from release.info import readBranchConfig
 from release.l10n import parsePlainL10nChangesets
 from release.versions import getAppVersion
+from release.platforms import buildbot2ftp
 from releasetasks import make_task_graph
 from taskcluster import Scheduler, Index
 from taskcluster.utils import slugId
@@ -163,7 +164,8 @@ def get_en_US_config(release, branchConfig, branch, index):
             platform=platform,
         ))
         platforms[platform] = {
-            "task_id": task["taskId"]
+            "task_id": task["taskId"],
+            "upload_platform": buildbot2ftp(platform),
         }
 
     return {
@@ -220,6 +222,8 @@ def main(options):
     configs_workdir = 'buildbot-configs'
     balrog_username = get_config(config, "balrog", "username", None)
     balrog_password = get_config(config, "balrog", "password", None)
+    beetmover_aws_access_key_id = get_config(config, "beetmover", "aws_access_key_id", None)
+    beetmover_aws_secret_access_key = get_config(config, "beetmover", "aws_secret_access_key", None)
 
     # TODO: replace release sanity with direct checks of en-US and l10n revisions (and other things if needed)
 
@@ -285,11 +289,14 @@ def main(options):
                 "balrog_api_root": branchConfig["balrog_api_root"],
                 "balrog_username": balrog_username,
                 "balrog_password": balrog_password,
+                "beetmover_aws_access_key_id": beetmover_aws_access_key_id,
+                "beetmover_aws_secret_access_key": beetmover_aws_secret_access_key,
                 # TODO: stagin specific, make them configurable
                 "signing_class": "dep-signing",
                 "bouncer_enabled": branchConfig["bouncer_enabled"],
                 "release_channels": branchConfig["release_channels"],
                 "signing_pvt_key": signing_pvt_key,
+                "push_to_candidates_enabled": branchConfig['push_to_candidates_enabled']
             }
 
             validate_graph_kwargs(**kwargs)
