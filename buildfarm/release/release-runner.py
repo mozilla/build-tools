a006c0916a065e784347ecafc4ef2efe7bbfd890
Justin Wood <Callek@gmail.com>
2016-02-25 19:14:40 -0500

    Bug 1251468 - Expose the ability to set mozharness revision (as a distinct rev besides the used-build) in release promotion. r=rail
    --HG--
    extra : rebase_source : 020021f66bde79ef95b3760157d183826b7d19be

--- buildfarm/release/release-runner.py
+++ buildfarm/release/release-runner.py
@@ -303,6 +303,8 @@ def main(options):
                 "repo_path": release["branch"],
                 "revision": release["mozillaRevision"],
                 "product": release["product"],
+                # if mozharness_revision is not passed, use 'revision'
+                "mozharness_changeset": release.get('mh_changeset', release['mozillaRevision']),
                 "partial_updates": getPartials(release),
                 "branch": branch,
                 "updates_enabled": bool(release["partials"]),
