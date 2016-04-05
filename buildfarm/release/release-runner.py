afbfd492087f48783dc0c7786f4329e864cc40d5
Rail Aliiev <rail@mozilla.com>
2016-04-05 00:38:14 -0400

    Bug 1260415 - tools: schedule partner repacks r=jlund
    MozReview-Commit-ID: JNUsM69hQ64

--- buildfarm/release/release-runner.py
+++ buildfarm/release/release-runner.py
@@ -608,6 +608,7 @@ def main(options):
                 "push_to_releases_enabled": push_to_releases_enabled,
                 "push_to_releases_automatic": branchConfig['push_to_releases_automatic'],
                 "beetmover_candidates_bucket": branchConfig["beetmover_buckets"][release["product"]],
+                "partner_repacks_platforms": branchConfig.get("partner_repacks_platforms", []),
             }
             if extra_balrog_submitter_params:
                 kwargs["extra_balrog_submitter_params"] = extra_balrog_submitter_params
