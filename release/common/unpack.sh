9035eb8398e4f71a037a556ee4798caf8f34ad62
Ben Hearsum <bhearsum@mozilla.com>
2018-02-16 17:07:52 -0500

    bug 1438906: quote more things in the update verify scripts to get rid of some non-fatal errors. r=rail

--- release/common/unpack.sh
+++ release/common/unpack.sh
@@ -37,8 +37,8 @@ unpack_build () {
                 fi
                 unpack_dir=`ls -1`
                 mv "${unpack_dir}"/*.app .
-                rm -rf $unpack_dir
-                appdir=`ls -1`
+                rm -rf "$unpack_dir"
+                appdir=`ls -1d *.app`
                 # The updater guesses the location of these files based on
                 # its own target architecture, not the mar. If we're not
                 # unpacking mac-on-mac, we need to copy them so it can find
